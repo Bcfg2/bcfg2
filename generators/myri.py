eaa0d649a5d86c432d12f4cf1d42d35b02647773
Narayan Desai <desai@mcs.anl.gov>
2004-06-09 18:36:20 +0000

    update to new API
    2004/06/08 09:51:08-05:00 anl.gov!desai
    update to use metadata api
    
    (Logical change 1.21)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@89 ce84e21b-d406-0410-9b95-82705330c041

--- generators/myri.py
+++ generators/myri.py
@@ -9,11 +9,14 @@ class myri(Generator):
     __name__ = 'myri'
     __version__ = '$Id$'
     __author__ = 'bcfg-dev@mcs.anl.gov'
-    __provides__ = {'ConfigFile':{'/etc/sysconfig/network-scripts/ifcfg-myri0':'build_myri'}}
 
     filedata="DEVICE=myri0\nIPADDR=%s\nBROADCAST=140.221.69.255\nNETMASK=255.255.254.0\nNETWORK=140.221.68.0\nONBOOT=yes\nBOOTPROTO=none"
 
-    def build_myri(self,name,client):
+    def __setup__(self):
+        self.__provides__ = {'ConfigFile':{'/etc/sysconfig/network-scripts/ifcfg-myri0':self.build_myri}}
+
+    def build_myri(self,name,metadata):
+        client = metadata.hostname
         (ip,port) = getaddrinfo(client,None)[0][4]
         subnet = int(ip.split('.')[2])+2
         lo = ip.split('.')[3]
