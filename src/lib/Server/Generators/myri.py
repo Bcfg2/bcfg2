43dbb4c68aa84fd243de1b545d0b57e11a45ac52
Narayan Desai <desai@mcs.anl.gov>
2004-08-11 20:40:57 +0000

    Rename: generators/myri.py -> src/lib/Server/Generators/myri.py
    (Logical change 1.37)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@208 ce84e21b-d406-0410-9b95-82705330c041

---
+++ src/lib/Server/Generators/myri.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from socket import getaddrinfo
+
+from Generator import Generator
+from Types import ConfigFile
+
+class myri(Generator):
+    __name__ = 'myri'
+    __version__ = '$Id$'
+    __author__ = 'bcfg-dev@mcs.anl.gov'
+
+    filedata="DEVICE=myri0\nIPADDR=%s\nBROADCAST=140.221.69.255\nNETMASK=255.255.254.0\nNETWORK=140.221.68.0\nONBOOT=yes\nBOOTPROTO=none"
+
+    def __setup__(self):
+        self.__provides__ = {'ConfigFile':{'/etc/sysconfig/network-scripts/ifcfg-myri0':self.build_myri}}
+
+    def build_myri(self,name,metadata):
+        client = metadata.hostname
+        (ip,port) = getaddrinfo(client,None)[0][4]
+        subnet = int(ip.split('.')[2])+2
+        lo = ip.split('.')[3]
+        addr = "140.221.%s.%s"%(subnet,lo)
+        return ConfigFile('/etc/sysconfig/network-scripts/ifcfg-myri0','root','root','0755',self.filedata%(addr))
