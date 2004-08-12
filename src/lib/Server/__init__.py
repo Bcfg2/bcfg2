da1f94935b16508c79dacbc44f9dd32dbb232616
Narayan Desai <desai@mcs.anl.gov>
2004-08-12 16:04:59 +0000

    add Structures
    (Logical change 1.39)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@231 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/__init__.py
+++ src/lib/Server/__init__.py
@@ -1,4 +1,4 @@
 # $Id: $
 
-__all__ = ["Core", "Generator", "Metadata", "Error", "GeneratorUtils", "Types", "Generators"] 
+__all__ = ["Core", "Generator", "Metadata", "Error", "GeneratorUtils", "Types", "Generators", "Structures"] 
 
