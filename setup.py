53bbbcb0baccd0a9d8b67265ccb07b0dcbf667b9
Narayan Desai <desai@mcs.anl.gov>
2004-08-12 15:39:34 +0000

    add generators and structures to build process
    (Logical change 1.38)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@228 ce84e21b-d406-0410-9b95-82705330c041

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name="Bcfg2.Server",
       description="Bcfg2 Server",
       author="Narayan Desai",
       author_email="desai@mcs.anl.gov",
-      packages=["Bcfg2", 'Bcfg2.Server', "Bcfg2.Client"],
+      packages=["Bcfg2", 'Bcfg2.Server', "Bcfg2.Server.Generators", "Bcfg2.Server.Structures", "Bcfg2.Client"],
       package_dir = {'Bcfg2':'src/lib'}, 
       scripts = ['src/sbin/Bcfg2Server', 'src/sbin/bcfg2']
      )
