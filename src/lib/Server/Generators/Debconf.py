ab37c6ac438cbfdfe049c3dc40842438394f19a5
Narayan Desai <desai@mcs.anl.gov>
2004-10-19 18:22:16 +0000

    updates
    2004/10/19 13:19:25-05:00 anl.gov!desai
    
    (Logical change 1.100)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@458 ce84e21b-d406-0410-9b95-82705330c041

---
+++ src/lib/Server/Generators/Debconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+'''this generator handles production of client-specific debconf files'''
+__revision__ = '$Revision$'
+
+from Bcfg2.Server.Generator import Generator, DirectoryBacked
+from elementtree.ElementTree import XML
+
+class Debconf(Generator):
+    '''Debconf takes <data>/template.dat and adds entries for
+    -> hostname
+    -> video driver'''
+    __name__ = 'Debconf'
+    __version__ = '$Revision$'
+    __author__ = 'bcfg-dev@mcs.anl.gov'
+    __provides__ = {'ConfigFile':{}}
+
+    probes = [XML('''<probe interpreter='/bin/sh'>lspci|grep VGA</probe>''')]
+
+    def __setup__(self):
+        self.repo = DirectoryBacked(self.data, self.core.fam)
+        self.xsensed = {}
+
+    def build_config_dat(self, entry, metadata):
+        '''build debconf file for client'''
+        entry.attrib['owner'] = 'root'
+        entry.attrib['group'] = 'root'
+        entry.attrib['perms'] = '0600'
+        filedata = self.repo.entries['config.dat']
+        xdriver = self.xsensed.get(metadata.hostname, "vesa")
+        entry.text = filedata % (metadata.hostname, xdriver)
+
+    def get_probes(self, metadata):
+        return self.probes
+
+    
+
+    
