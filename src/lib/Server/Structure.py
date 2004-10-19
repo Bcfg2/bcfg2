4aaa9edef9d011e8ab3e3ff69c9bcef708db9375
Narayan Desai <desai@mcs.anl.gov>
2004-10-19 20:30:03 +0000

    add revision field
    2004/10/19 14:15:54-05:00 anl.gov!desai
    add __name__ field
    
    (Logical change 1.103)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@470 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/Structure.py
+++ src/lib/Server/Structure.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-# $Id: $
+
+__revision__ = '$Revision$'
 
 class Structure(object):
     '''The Structure class is used to define patterns of data in host configurations
     Structure subtyped classes provide functions that group configurations into dependent
     and independent clauses'''
+    __name__ = 'example'
 
     def __init__(self, core, datastore):
         self.data = "%s/%s"%(datastore,self.__name__)
