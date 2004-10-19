f343ccc7f429f118f344334efa768fee4946f1d3
Narayan Desai <desai@mcs.anl.gov>
2004-10-19 19:09:17 +0000

    drop myri
    (Logical change 1.101)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@461 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/Generators/__init__.py
+++ src/lib/Server/Generators/__init__.py
@@ -3,4 +3,4 @@
 '''This is the skeleton file that describes all generators'''
 __revision__ = '$Revision$'
 
-all = ['Chiba', 'Debconf', 'account', 'cfg', 'myri', 'pkgmgr', 'servicemgr', 'sshbase']
+all = ['Chiba', 'Debconf', 'account', 'cfg', 'pkgmgr', 'servicemgr', 'sshbase']
