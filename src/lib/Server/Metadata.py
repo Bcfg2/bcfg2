240440b706864703b8683ced141dae0b11f7ed66
Narayan Desai <desai@mcs.anl.gov>
2004-10-21 20:39:09 +0000

    fix __cmp__
    (Logical change 1.106)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@479 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/Metadata.py
+++ src/lib/Server/Metadata.py
@@ -36,6 +36,12 @@ class Metadata(object):
             return False
         return True
 
+    def __cmp__(self, other):
+        fields = ['image', 'classes', 'bundles', 'attributes', 'hostname']
+        sum1 = [x for x in len(fields) if getattr(self, fields[x])][0]
+        sum2 = [x for x in len(fields) if getattr(other, fields[x])][0]
+        return sum1 < sum2
+
 class Profile(object):
     def __init__(self, xml):
         object.__init__(self)
