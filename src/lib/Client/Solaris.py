07b432faf631d20565b334f1955191fa85106b8a
Narayan Desai <desai@mcs.anl.gov>
2004-10-26 21:56:28 +0000

    make into a real stub
    (Logical change 1.118)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@519 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Client/Solaris.py
+++ src/lib/Client/Solaris.py
@@ -4,23 +4,6 @@ __revision__ = '$Revision$'
 
 from Bcfg2.Client.Toolset import Toolset
 
-def Detect():
-    # until the code works
-    return False
-
 class Solaris(Toolset):
     '''This class implelements support for SYSV packages and standard /etc/init.d services'''
-
-    def VerifyService(self, entry):
-        return False
-
-    def VerifyPackage(self, entry):
-        return False
-
-    def InstallService(self, entry):
-        return False
-
-    def InstallPackage(self, entry):
-        return False
-
-
+    pass
