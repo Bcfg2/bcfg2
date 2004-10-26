7516712a772f6d69d17d1c783563e42ea5c489fa
Narayan Desai <desai@mcs.anl.gov>
2004-10-26 21:56:28 +0000

    make the stub really a stub
    (Logical change 1.118)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@521 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Client/Gentoo.py
+++ src/lib/Client/Gentoo.py
@@ -4,23 +4,6 @@ __revision__ = '$Revision$'
 
 from Bcfg2.Client.Toolset import Toolset
 
-def Detect():
-    # until the code works
-    return False
-
 class Gentoo(Toolset):
     '''This class implelements support for emerge packages and standard rc-update services'''
-
-    def VerifyService(self, entry):
-        return False
-
-    def VerifyPackage(self, entry):
-        return False
-
-    def InstallService(self, entry):
-        return False
-
-    def InstallPackage(self, entry):
-        return False
-
-
+    pass
