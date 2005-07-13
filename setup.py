1f534dcf8bcb00a2bf1bed373689d07c2886443f
Narayan Desai <desai@mcs.anl.gov>
2005-07-13 19:26:11 +0000

    add report stuff mv ValidateBcfg2Repo
    (Logical change 1.257)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@1056 ce84e21b-d406-0410-9b95-82705330c041

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name="Bcfg2.Server",
       author_email="desai@mcs.anl.gov",
       packages=["Bcfg2", 'Bcfg2.Server', "Bcfg2.Server.Generators", "Bcfg2.Server.Structures", "Bcfg2.Client"],
       package_dir = {'Bcfg2':'src/lib'}, 
-      scripts = ['src/sbin/Bcfg2Server', 'src/sbin/bcfg2', 'tools/ValidateBcfg2Repo'],
-      data_files = [('share/bcfg2/schemas', ['schemas/atom.xsd', 'schemas/base.xsd', 'schemas/bundle.xsd', 'schemas/metadata.xsd', 'schemas/pkglist.xsd', 'schemas/services.xsd', 'schemas/translation.xsd']), ('share/man/man1', ['man/bcfg2.1']), ('share/man/man8', ['man/Bcfg2Server.8', 'man/ValidateBcfg2Repo.8'])]
+      scripts = ['src/sbin/Bcfg2Server', 'src/sbin/bcfg2', 'src/sbin/ValidateBcfg2Repo'],
+      data_files = [('share/bcfg2/schemas', ['schemas/atom.xsd', 'schemas/base.xsd', 'schemas/bundle.xsd', 'schemas/metadata.xsd', 'schemas/pkglist.xsd', 'schemas/services.xsd', 'schemas/translation.xsd', 'schemas/report-configuration.xsd']), ('share/man/man1', ['man/bcfg2.1']), ('share/man/man8', ['man/Bcfg2Server.8', 'man/ValidateBcfg2Repo.8'])]
      )
