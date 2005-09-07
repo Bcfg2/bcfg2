228a6896227dde1c2e1ac4d6b27d5d7aee42f313
Narayan Desai <desai@mcs.anl.gov>
2005-09-07 17:35:15 +0000

    Delete: src/lib/Server/Generators/__init__.py
    }(Logical change 1.300)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@1199 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/Generators/__init__.py
+++
@@ -1,6 +0,0 @@
-# $Id: $
-
-'''This is the skeleton file that describes all generators'''
-__revision__ = '$Revision$'
-
-all = ['Chiba', 'Debconf', 'account', 'cfg', 'pkgmgr', 'servicemgr', 'sshbase']
