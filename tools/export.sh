22eeb364283bee5b33d3451b9a4489b0d230d971
Narayan Desai <desai@mcs.anl.gov>
2005-10-18 23:00:21 +0000

    Change mode to -rwxr-xr-x
    2005/10/18 17:58:25-05:00 anl.gov!desai
    
    (Logical change 1.340)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@1398 ce84e21b-d406-0410-9b95-82705330c041

---
+++ tools/export.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+version="${1}"
+
+if [ -z "$version" ] ; then
+    echo "must supply version number"
+    exit 1
+fi
+
+bk export . /tmp/bcfg2-${version}
+bk changes -aeR > /tmp/bcfg2-${version}/ChangeLog
+cd /tmp/bcfg2-${version}/doc
+make
+cd /tmp
+tar czf bcfg2-${version}.tar.gz bcfg2-${version}
