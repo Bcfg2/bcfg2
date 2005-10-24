45656bc95cdd2bfb943cba5929db9f62ada3c611
Narayan Desai <desai@mcs.anl.gov>
2005-10-24 08:47:26 +0000

    add Cobalt Component code so that Bcfg2 can be standalone
    (Logical change 1.341)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@1406 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/__init__.py
+++ src/lib/Server/__init__.py
@@ -2,5 +2,5 @@
 '''This is the set of modules for Bcfg2.Server'''
 
 __revision__ = '$Revision$'
-__all__ = ["Core", "Metadata", "Plugin", "Plugins", "Statistics"] 
+__all__ = ["Component", "Core", "Metadata", "Plugin", "Plugins", "Statistics"] 
 
