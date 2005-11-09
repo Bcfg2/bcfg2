6a340f1184a61c300ef4e8c3ba6e2c982f247671
Narayan Desai <desai@mcs.anl.gov>
2005-11-09 20:58:08 +0000

    add package signing and server upload
    (Logical change 1.352)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@1467 ce84e21b-d406-0410-9b95-82705330c041

--- tools/export.sh
+++ tools/export.sh
@@ -13,3 +13,5 @@ cd /tmp/bcfg2-${version}/doc
 make
 cd /tmp
 tar czf bcfg2-${version}.tar.gz bcfg2-${version}
+gpg --sign /tmp/bcfg2-${version}.tar.gz
+scp /tmp/bcfg2-${version}.tar.gz* terra.mcs.anl.gov:/nfs/ftp/pub/bcfg
