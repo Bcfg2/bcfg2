b4451305f62af49c989517bfcafd5f0912348bbd
Narayan Desai <desai@mcs.anl.gov>
2005-11-09 21:59:43 +0000

    add web export as well
    (Logical change 1.355)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@1481 ce84e21b-d406-0410-9b95-82705330c041

--- tools/export.sh
+++ tools/export.sh
@@ -15,3 +15,4 @@ cd /tmp
 tar czf bcfg2-${version}.tar.gz bcfg2-${version}
 gpg --sign /tmp/bcfg2-${version}.tar.gz
 scp /tmp/bcfg2-${version}.tar.gz* terra.mcs.anl.gov:/nfs/ftp/pub/bcfg
+scp /tmp/bcfg2-${version}.tar.gz* terra.mcs.anl.gov:/nfs/www-space-004/cobalt/bcfg2
