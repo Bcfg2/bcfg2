d25eb75b8f634e8122245c37a144ad7be9006a5c
Narayan Desai <desai@mcs.anl.gov>
2005-11-29 20:00:54 +0000

    fix lxml brokenness
    (Logical change 1.372)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@1568 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/Plugins/Account.py
+++ src/lib/Server/Plugins/Account.py
@@ -50,12 +50,10 @@ class Account(Plugin):
 
     def gen_root_keys_cb(self, entry, metadata):
         '''Build root authorized keys file based on current ACLs'''
-        entry.text = ''
         superusers = self.repository.entries['superusers'].data.split()
         rootlike = [line.split(':', 1) for line in self.repository.entries['rootlike'].data.split()]
         superusers += [user for (user, host) in rootlike if host == metadata.hostname.split('.')[0]]
-        for user in superusers:
-            if self.repository.entries.has_key("%s.key" % user):
-                entry.text += self.repository.entries["%s.key" % user].data
+        rdata = self.repository.entries
+        entry.text = "".join([rdata[user] for user in superusers if rdata.has_key(user)])
         perms = {'owner':'root', 'group':'root', 'perms':'0600'}
         [entry.attrib.__setitem__(key, value) for (key, value) in perms.iteritems()]
