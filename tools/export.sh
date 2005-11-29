0339b8bd040d8864ae099ab82a91f5b681ea7748
Narayan Desai <desai@mcs.anl.gov>
2005-11-29 20:00:54 +0000

    fix gpg call
    (Logical change 1.372)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@1570 ce84e21b-d406-0410-9b95-82705330c041

--- tools/export.sh
+++ tools/export.sh
@@ -12,7 +12,8 @@ bk changes -aer > /tmp/bcfg2-${version}/ChangeLog
 cd /tmp/bcfg2-${version}/doc
 make
 cd /tmp
-tar czf bcfg2-${version}.tar.gz bcfg2-${version}
-gpg --sign /tmp/bcfg2-${version}.tar.gz
+filename="/tmp/bcfg2-${version}.tar.gz"
+tar czf "${filename}" bcfg2-${version}
+gpg --armor --output "${filename}".gpg --detach-sig "${filename}"
 scp /tmp/bcfg2-${version}.tar.gz* terra.mcs.anl.gov:/nfs/ftp/pub/bcfg
 scp /tmp/bcfg2-${version}.tar.gz* terra.mcs.anl.gov:/nfs/www-space-004/cobalt/bcfg2
