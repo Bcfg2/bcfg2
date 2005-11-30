989e7dfacd34f4a47d26d0e0b05ad758dd43ce84
Narayan Desai <desai@mcs.anl.gov>
2005-11-30 17:02:37 +0000

    remove xmlrpccomm from the module list
    (Logical change 1.375)
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@1577 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Client/__init__.py
+++ src/lib/Client/__init__.py
@@ -1,4 +1,4 @@
 '''This contains all Bcfg2 Client modules'''
 __revision__ = '$Revision$'
 
-__all__ = ["Toolset", "Debian", "Solaris", "Redhat", "Gentoo", "XMLRPCComm"]
+__all__ = ["Toolset", "Debian", "Solaris", "Redhat", "Gentoo"]
