e175860dd5a6aaab8597b5244b6063e341d016da
Narayan Desai <desai@mcs.anl.gov>
2005-12-01 18:58:05 +0000

    fix export script for svn
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@1602 ce84e21b-d406-0410-9b95-82705330c041

--- tools/export.sh
+++ tools/export.sh
@@ -7,8 +7,8 @@ if [ -z "$version" ] ; then
     exit 1
 fi
 
-bk export . /tmp/bcfg2-${version}
-bk changes -aer > /tmp/bcfg2-${version}/ChangeLog
+svn export . /tmp/bcfg2-${version}
+svn log -v > /tmp/bcfg2-${version}/ChangeLog
 cd /tmp/bcfg2-${version}/doc
 make
 cd /tmp
