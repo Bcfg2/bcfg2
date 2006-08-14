6f83d1d62ce9a8fa56db8f7966eae216d538adf4
Narayan Desai <desai@mcs.anl.gov>
2006-08-14 15:20:23 +0000

    * Make all XML methods uniform across drivers  * Switch bcfg2 to using the XML abstraction layer
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@2064 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Client/XML.py
+++ src/lib/Client/XML.py
@@ -4,18 +4,18 @@ __revision__ = '$Revision$'
 # library will use lxml, then builtin xml.etree, then ElementTree
 
 try:
-    from lxml.etree import Element, SubElement, tostring
+    from lxml.etree import Element, SubElement, XML, tostring
     from lxml.etree import XMLSyntaxError as ParseError
     driver = 'lxml'
 except ImportError:
     # lxml not available 
     try:
-        from xml.etree.ElementTree import Element, XML, tostring
+        from xml.etree.ElementTree import Element, SubElement, XML, tostring
         from xml.parsers.expat import ExpatError as ParseError
         driver = 'etree-py'
     except ImportError:
         try:
-            from elementtree.ElementTree import Element, XML, tostring
+            from elementtree.ElementTree import Element, SubElement, XML, tostring
             from xml.parsers.expat import ExpatError as ParseError
             driver = 'etree'
         except ImportError:
