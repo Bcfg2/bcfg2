ed4fcdc39ddea985cf893fdc147c5ee294dfbce2
David Dahl <dahl@mcs.anl.gov>
2006-10-13 16:59:28 +0000

    removed hard-coded path to Hostbase module in test harness
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@2433 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/Hostbase/test/harness.py
+++ src/lib/Server/Hostbase/test/harness.py
@@ -6,4 +6,6 @@ server_hostbase = os.path.realpath(here + '../../../../')
 
 sys.path.insert(0,server_hostbase)
 sys.path.insert(0,server_hostbase + '../')
-sys.path.insert(0,'/home/dahl/Code/bcfg2/src/lib/Server/Hostbase')
+#commented this out, but might be needed for now until the harness is figured out
+#if so, use your actual path to the Hostbase module
+#sys.path.insert(0,'/home/dahl/Code/bcfg2/src/lib/Server/Hostbase')
