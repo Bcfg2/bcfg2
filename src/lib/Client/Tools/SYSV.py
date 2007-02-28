1e065eb039b75d8f36ccbd58bbeec71e699994bc
Narayan Desai <desai@mcs.anl.gov>
2007-02-28 01:25:51 +0000

    Fix a cosmetic traceback
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@2888 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Client/Tools/SYSV.py
+++ src/lib/Client/Tools/SYSV.py
@@ -57,9 +57,12 @@ class SYSV(Bcfg2.Client.Tools.PkgTool):
                              (entry.get('version'), entry.get('name')))[0]
 
         if cmdrc != 0:
-            self.logger.debug("Package %s version incorrect: have %s want %s" \
-                              % (entry.get('name'), self.installed[entry.get('name')],
-                                 entry.get('version')))
+            if entry.get('name') in self.installed:
+                self.logger.debug("Package %s version incorrect: have %s want %s" \
+                                  % (entry.get('name'), self.installed[entry.get('name')],
+                                     entry.get('version')))
+            else:
+                self.logger.debug("Package %s not installed" % (entry.get("name")))
         else:
             if self.setup['quick'] or entry.attrib.get('verify', 'true') == 'false':
                 return True
