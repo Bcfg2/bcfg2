42c3c94f3d6008ffbf4de25cf17ed81c21996fb0
Narayan Desai <desai@mcs.anl.gov>
2007-06-14 15:21:50 +0000

    Change TCheetah to set self.path for config file path
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@3302 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/Plugins/TCheetah.py
+++ src/lib/Server/Plugins/TCheetah.py
@@ -46,7 +46,7 @@ class TemplateFile:
     def BuildFile(self, entry, metadata):
         '''Build literal file information'''
         self.template.metadata = metadata
-        self.template.entry = entry
+        self.template.path = entry.get('name')
         self.template.properties = self.properties.properties
         try:
             entry.text = str(self.template)
