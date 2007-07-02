c72be0ec4f137f9fee9fa7621461ab3088e999a3
Daniel Joseph Barnhart Clark <dclark@pobox.com>
2007-07-02 16:15:05 +0000

    Removed because we no longer use built-in Python SSL (we use tlslite instead).
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@3400 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Client/Proxy.py
+++ src/lib/Client/Proxy.py
@@ -139,9 +139,6 @@ class SafeProxy:
             except xmlrpclib.Fault:
                 self.log.debug("Operation %s completed with fault" % (methodName))
                 raise
-            except socket.sslerror:
-                self.log.debug("Attempt %d of %d failed due to SSL negotiation failure" %
-                               ((irs + 1), self._retries))
             except socket.error, serr:
                 self.log.debug("Attempting %s (%d of %d) failed because %s" % (methodName, (irs+1),
                                                                                self._retries, serr))
