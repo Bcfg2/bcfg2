b675fc60be84b2300a37a62752347b5a233e1e1f
Narayan Desai <desai@mcs.anl.gov>
2007-11-03 23:32:17 +0000

    Fix __all__
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@3910 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/Plugins/__init__.py
+++ src/lib/Server/Plugins/__init__.py
@@ -1,6 +1,6 @@
 '''imports for Bcfg2.Server.Plugins'''
 __revision__ = '$Revision$'
 
-all = ['Account', 'Base', 'Bundler', 'Cfg', 'Hostbase', 'Metadata',
-       'Pkgmgr', 'Rules', 'SSHbase', 'Svcmgr', 'TCheetah',
-       'SGenshi', 'TGenshi', 'Vhost']
+__all__ = ['Account', 'Base', 'Bundler', 'Cfg', 'Hostbase', 'Metadata',
+           'Pkgmgr', 'Rules', 'SSHbase', 'Svcmgr', 'TCheetah',
+           'SGenshi', 'TGenshi', 'Vhost']
