64bda9aa65fb97254430819329675cb040580210
Narayan Desai <desai@mcs.anl.gov>
2007-12-02 02:43:53 +0000

    Fix export.sh to perform tagging appropriately for non-trunk trees [bugfix]
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@4028 ce84e21b-d406-0410-9b95-82705330c041

--- tools/export.sh
+++ tools/export.sh
@@ -5,13 +5,14 @@ repo="https://svn.mcs.anl.gov/repos/bcfg"
 version="${1}"
 expath="/tmp/${name}-${version}/"
 tarname="/tmp/${name}-${version}.tar.gz"
+url=`svn info | grep URL | awk '{print $2}'`
 
 if [ -z "$version" ] ; then
     echo "must supply version number"
     exit 1
 fi
 tagstr=`echo ${version} | sed -e 's/\./_/g'`
-svn copy "${repo}/trunk" "${repo}/tags/${name}_${tagstr}" -m "tagged ${version} release"
+svn copy "$url" "${repo}/tags/${name}_${tagstr}" -m "tagged ${version} release"
 svn export . "${expath}"
 svn log -v "${repo}/tags/${name}_${tagstr}" > "${expath}/ChangeLog"
 cd "${expath}"/doc && make all
