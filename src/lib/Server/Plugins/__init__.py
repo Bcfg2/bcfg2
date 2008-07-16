bfb15fea0dfd42d8f0f23a30c716c92e04169b89
Andrew Brestick <brestick@mcs.anl.gov>
2008-07-16 21:14:39 +0000

    bb plugin will be off by default
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@4800 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/Plugins/__init__.py
+++ src/lib/Server/Plugins/__init__.py
@@ -1,6 +1,6 @@
 '''imports for Bcfg2.Server.Plugins'''
 __revision__ = '$Revision$'
 
-__all__ = ['Account', 'Base', 'BB', 'Bundler', 'Cfg', 'Hostbase', 'Metadata',
+__all__ = ['Account', 'Base', 'Bundler', 'Cfg', 'Hostbase', 'Metadata',
            'Pkgmgr', 'Rules', 'SSHbase', 'Svcmgr', 'TCheetah',
            'SGenshi', 'TGenshi', 'Vhost']
