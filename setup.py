d68db2c50a203770a93c90e76d407fbd5e12cd8e
Narayan Desai <desai@mcs.anl.gov>
2008-09-01 12:28:43 +0000

    bump setuptools version to 0.9.6pre2
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@4898 ce84e21b-d406-0410-9b95-82705330c041

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from glob import glob
 
 setup(name="Bcfg2",
-      version="0.9.6pre1",
+      version="0.9.6pre2",
       description="Bcfg2 Server",
       author="Narayan Desai",
       author_email="desai@mcs.anl.gov",
