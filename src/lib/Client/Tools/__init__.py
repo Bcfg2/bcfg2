60a2643d4e7bd0a0964180ebbfd8e31d12cbbf98
Narayan Desai <desai@mcs.anl.gov>
2009-05-15 03:21:34 +0000

    Remove deprecated RPM and Yum drivers
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@5237 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Client/Tools/__init__.py
+++ src/lib/Client/Tools/__init__.py
@@ -3,8 +3,7 @@ __revision__ = '$Revision$'
 
 __all__ = ["Action", "APT", "Blast", "Chkconfig", "DebInit", "Encap", "IPS",
            "FreeBSDInit", "FreeBSDPackage", "launchd", "Portage", "POSIX",
-           "RPM", "RPMng", 'rpmtools', "RcUpdate", "SMF", "SYSV", "Yum",
-           "YUMng"]
+           "RPMng", 'rpmtools', "RcUpdate", "SMF", "SYSV", "YUMng"]
 
 drivers = [item for item in __all__ if item not in ['rpmtools']]
 default = [item for item in drivers if item not in ['RPM', 'Yum']]
