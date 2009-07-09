7c9205aafd495ae053b899ad900cdd40913566a2
Narayan Desai <desai@mcs.anl.gov>
2009-07-09 01:56:22 +0000

    Trigger: Add Trigger plugin
    Implement Trigger plugin. Can be used to call out when clients upload
    statistics packets. This plugin will call each executable within the
    Trigger spool directory, passing <hostname> -p <profile> -g <grouplist>
    The group list is delimited with colons.
    
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@5316 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/Plugins/__init__.py
+++ src/lib/Server/Plugins/__init__.py
@@ -4,4 +4,4 @@ __revision__ = '$Revision$'
 __all__ = ['Account', 'Base', 'Bundler', 'Cfg', 'Decisions', 'GBundler', 'Git',
            'Hostbase', 'Metadata', 'NagiosGen', 'Packages', 'Properties',
            'Probes', 'Pkgmgr', 'Rules', 'SSHbase', 'Snapshots' 'Statistics',
-	   'Svcmgr', 'Svn', 'TCheetah', 'SGenshi', 'TGenshi', 'Vhost']
+	   'Svcmgr', 'Svn', 'TCheetah', 'Trigger', 'SGenshi', 'TGenshi', 'Vhost']
