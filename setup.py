44af80804e5d5e6d8254e7ca714a3df340d7dd2e
Narayan Desai <desai@mcs.anl.gov>
2009-11-01 02:41:05 +0000

    Version bump to 1.0.0rc2
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@5519 ce84e21b-d406-0410-9b95-82705330c041

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from glob import glob
 
 setup(name="Bcfg2",
-      version="1.0.0rc1",
+      version="1.0.0rc2",
       description="Bcfg2 Server",
       author="Narayan Desai",
       author_email="desai@mcs.anl.gov",
