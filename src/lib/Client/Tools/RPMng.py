ebdbc22d327a6c33794a0b62745bbbcb6893f943
Narayan Desai <desai@mcs.anl.gov>
2009-12-09 17:22:59 +0000

    RPMng: fix ignore filename handing (Resolves Ticket #811)
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@5617 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Client/Tools/RPMng.py
+++ src/lib/Client/Tools/RPMng.py
@@ -363,11 +363,10 @@ class RPMng(Bcfg2.Client.Tools.PkgTool):
                                 self.instance_status[inst]['verify_fail'] = True
 
                             # Check the rpm verify file results against the modlist
-                            # and per Instance Ignores.
+                            # and entry and per Instance Ignores.
+                            ignores = [ig.get('name') for ig in entry.findall('Ignore')] + [ig.get('name') for ig in inst.findall('Ignore')]
                             for file_result in result.get('files', []):
-                                if file_result[-1] not in modlist + \
-                                       [ignore.get('name') for ignore \
-                                        in inst.findall('Ignore')]:
+                                if file_result[-1] not in modlist + ignores:
                                     instance_fail = True
                                     self.instance_status[inst]['verify_fail'] = True
                                 else:
