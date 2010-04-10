37b8717f178aa7b4037d9607ef567a7c85014fc9
Sol Jerome <solj@ices.utexas.edu>
2010-04-10 16:45:51 -0500

    Hostbase/settings.py: Reset language default back to en-us
    Signed-off-by: Sol Jerome <solj@ices.utexas.edu>

--- src/lib/Server/Hostbase/settings.py
+++ src/lib/Server/Hostbase/settings.py
@@ -74,7 +74,7 @@ SERVE_MEDIA = DEBUG
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
 # http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'en-us'
 SITE_ID = 1
 # URL that handles the media served from MEDIA_ROOT.
 # Example: "http://media.lawrence.com"
