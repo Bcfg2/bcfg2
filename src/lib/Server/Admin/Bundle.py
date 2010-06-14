1880631fd4a14f715252109d393fa6293c2cb172
Fabian Affolter <fabian@bernewireless.net>
2010-06-14 13:40:12 +0000

    Added missing configuration entities
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@5928 ce84e21b-d406-0410-9b95-82705330c041

--- src/lib/Server/Admin/Bundle.py
+++ src/lib/Server/Admin/Bundle.py
@@ -86,7 +86,7 @@ class Bundle(Bcfg2.Server.Admin.MetadataCore):
                     tree = lxml.etree.parse(bundle_list[int(lineno)])
                     #Print bundle content
                     #print lxml.etree.tostring(tree)
-                    names = ['ConfigFile', 'Package', 'Service']
+                    names = ['Action', 'ConfigFile', 'Directory', 'Package', 'Permission', 'Service', 'SymLink']
                     for name in names:
                         for node in tree.findall("//" + name):
                     	    print "%s:\t%s" % (name, node.attrib["name"])
