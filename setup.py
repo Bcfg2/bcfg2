9c092909ac21ac2cab170decdec4b840765012ef
Sol Jerome <solj@ices.utexas.edu>
2010-07-19 19:46:03 +0000

    Version bump to 1.1.0rc4
    git-svn-id: https://svn.mcs.anl.gov/repos/bcfg/trunk/bcfg2@5982 ce84e21b-d406-0410-9b95-82705330c041

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from glob import glob
 
 setup(cmdclass=cmdclass,
       name="Bcfg2",
-      version="1.1.0rc3",
+      version="1.1.0rc4",
       description="Bcfg2 Server",
       author="Narayan Desai",
       author_email="desai@mcs.anl.gov",
