e19cb1b654b393396d0c6f94691cb97e281a3408
Tim Laszlo <tim.laszlo@gmail.com>
2010-10-18 11:13:46 -0500

    web_reports: remove comments

--- src/lib/Server/Reports/urls.py
+++ src/lib/Server/Reports/urls.py
@@ -3,10 +3,6 @@ from django.http import HttpResponsePermanentRedirect
 
 handler500 = 'Bcfg2.Server.Reports.reports.views.server_error'
 
-#from ConfigParser import ConfigParser, NoSectionError, NoOptionError
-#c = ConfigParser()
-#c.read(['/etc/bcfg2.conf', '/etc/bcfg2-web.conf'])
-
 urlpatterns = patterns('',
     (r'^', include('Bcfg2.Server.Reports.reports.urls'))
 )
