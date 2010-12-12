725383528b3b7132644bacfa1d146e084a39277f
Fabian Affolter <fabian@bernewireless.net>
2010-12-12 10:11:40 +0100

    backup and bundle added

--- src/lib/Server/Admin/__init__.py
+++ src/lib/Server/Admin/__init__.py
@@ -1,8 +1,24 @@
 __revision__ = '$Revision$'
 
-__all__ = ['Mode', 'Client', 'Compare', 'Init', 'Minestruct', 'Perf',
-           'Pull', 'Query', 'Reports', 'Snapshots', 'Tidy', 'Viz', 
-           'Xcmd', 'Group', 'Backup']
+__all__ = [
+        'Backup',
+        'Bundle',
+        'Client',
+        'Compare',
+        'Group',
+        'Examples',
+        'Init',
+        'Minestruct',
+        'Mode',
+        'Perf',
+        'Pull',
+        'Query',
+        'Reports',
+        'Snapshots',
+        'Tidy',
+        'Viz',
+        'Xcmd'
+        ]
 
 import ConfigParser
 import logging
