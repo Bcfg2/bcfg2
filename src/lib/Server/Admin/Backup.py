b61afa3e10e9f6b1caa568bd9974e0e221a34f4b
Fabian Affolter <fabian@bernewireless.net>
2010-12-27 16:39:31 +0100

    Description fixed (cherry picked from commit 8acbe8220c4fec09073933a5e461a06c00d99b81)

--- src/lib/Server/Admin/Backup.py
+++ src/lib/Server/Admin/Backup.py
@@ -6,7 +6,7 @@ import Bcfg2.Server.Admin
 import Bcfg2.Options
 
 class Backup(Bcfg2.Server.Admin.MetadataCore):
-    __shorthelp__ = "Make a backup of the Bcfg2 repository."
+    __shorthelp__ = "Make a backup of the Bcfg2 repository"
     __longhelp__ = (__shorthelp__ + "\n\nbcfg2-admin backup")
                                     #"\n\nbcfg2-admin backup restore")
     __usage__ = ("bcfg2-admin backup")
