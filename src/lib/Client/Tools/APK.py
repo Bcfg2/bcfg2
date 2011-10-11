2f28cf4027f82e15180d5e022fda55699e0f0d61
Sol Jerome <sol.jerome@gmail.com>
2011-10-11 17:09:00 -0500

    APK: Pylint/PEP8 fixes
    Signed-off-by: Sol Jerome <sol.jerome@gmail.com>

--- src/lib/Client/Tools/APK.py
+++ src/lib/Client/Tools/APK.py
@@ -3,6 +3,7 @@ __revision__ = '$Revision$'
 
 import Bcfg2.Client.Tools
 
+
 class APK(Bcfg2.Client.Tools.PkgTool):
     """Support for Apk packages."""
     name = 'APK'
@@ -22,10 +23,11 @@ class APK(Bcfg2.Client.Tools.PkgTool):
         names = self.cmd.run("/sbin/apk info")[1]
         nameversions = self.cmd.run("/sbin/apk info -v")[1]
         for pkg in zip(names, nameversions):
-                pkgname = pkg[0]
-                version = pkg[1][len(pkgname)+1:]
-                self.logger.debug(" pkgname: %s\n version: %s" % (pkgname, version))
-                self.installed[pkgname] = version
+            pkgname = pkg[0]
+            version = pkg[1][len(pkgname) + 1:]
+            self.logger.debug(" pkgname: %s\n version: %s" %
+                              (pkgname, version))
+            self.installed[pkgname] = version
 
     def VerifyPackage(self, entry, modlist):
         """Verify Package status for entry."""
@@ -35,14 +37,16 @@ class APK(Bcfg2.Client.Tools.PkgTool):
             return False
 
         if entry.attrib['name'] in self.installed:
-            if entry.attrib['version'] == 'auto' or self.installed[entry.attrib['name']] == entry.attrib['version']:
+            if entry.attrib['version'] in ['auto', self.installed[entry.attrib['name']]]:
                 #if not self.setup['quick'] and \
                 #                entry.get('verify', 'true') == 'true':
-                #FIXME: We should be able to check this once
-                #       http://trac.macports.org/ticket/15709 is implemented
+                #FIXME: Does APK have any sort of verification mechanism?
                 return True
             else:
-		self.loggger.info( " pkg %s at version %s, not %s" % (entry.attrib['name'],self.installed[entry.attrib['name']],entry.attrib['version']) )
+                self.loggger.info(" pkg %s at version %s, not %s" %
+                                  (entry.attrib['name'],
+                                   self.installed[entry.attrib['name']],
+                                   entry.attrib['version']))
                 entry.set('current_version', self.installed[entry.get('name')])
                 return False
         entry.set('current_exists', 'false')
