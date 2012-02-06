4b8bdacc8f00d13e1ee4746ea54de2c8d35d10ae
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2012-02-06 14:29:25 -0500

    made PackagesConfig object more resilient on initial plugin loading

--- src/lib/Server/Plugins/Packages/PackagesConfig.py
+++ src/lib/Server/Plugins/Packages/PackagesConfig.py
@@ -7,7 +7,7 @@ class PackagesConfig(Bcfg2.Server.Plugin.SimpleConfig):
         """ Build local data structures """
         Bcfg2.Server.Plugin.SimpleConfig.Index(self)
 
-        if self.plugin.sources.loaded:
+        if hasattr(self.plugin, "sources") and self.plugin.sources.loaded:
             # only reload Packages plugin if sources have been loaded.
             # otherwise, this is getting called on server startup, and
             # we have to wait until all sources have been indexed
