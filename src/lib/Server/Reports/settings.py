49b703f33f20d2662864db5e86bbdb0add3e1f59
Calen Pennington <cpennington@wgen.net>
2012-03-06 11:59:03 -0500

    Don't set TIME_ZONE to None if django is less than 1.2

--- src/lib/Server/Reports/settings.py
+++ src/lib/Server/Reports/settings.py
@@ -76,7 +76,9 @@ if django.VERSION[0] == 1 and django.VERSION[1] < 2:
 
 # Local time zone for this installation. All choices can be found here:
 # http://docs.djangoproject.com/en/dev/ref/settings/#time-zone
-TIME_ZONE = args.statistics_time_zone
+if (args.statistics_time_zone is not None or
+    django.VERSION[0] == 1 and django.VERSION[1] > 2):
+    TIME_ZONE = args.statistics_time_zone
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
