fc5eb3232c2ca9479192b8fcff6e6c8c213ce291
Dan Foster <dan.foster@bristol.ac.uk>
2012-03-09 19:00:41 -0600

    solaris: Fix building on Solaris 10
    Signed-off-by: Sol Jerome <sol.jerome@gmail.com>

---
+++ solaris/gen-prototypes.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+cd build
+PP="./"`ls -1d lib/*`"/site-packages/"
+
+#bcfg2
+echo "i pkginfo=./pkginfo.bcfg2" >  ../prototype.tmp
+find . | grep man[15] | pkgproto >> ../prototype.tmp
+echo "./bin"  | pkgproto >> ../prototype.tmp
+echo "./bin/bcfg2"  | pkgproto >> ../prototype.tmp
+echo "${PP}Bcfg2" | pkgproto >> ../prototype.tmp
+ls -1 ${PP}Bcfg2/*.py  | pkgproto >> ../prototype.tmp
+find  ${PP}Bcfg2/Client/ ! -name "*.pyc"  | pkgproto >> ../prototype.tmp
+sed "s/`id | sed 's/uid=[0-9]*(\(.*\)) gid=[0-9]*(\(.*\))/\1 \2/'`/bin bin/" ../prototype.tmp > ../prototype.bcfg2
+
+#bcfg2-server
+echo "i pkginfo=./pkginfo.bcfg2-server" >  ../prototype.tmp
+find . | grep man8 | pkgproto >> ../prototype.tmp
+find share/bcfg2 | pkgproto >> ../prototype.tmp
+echo "./bin"  | pkgproto >> ../prototype.tmp
+ls -1 bin/bcfg2-* | pkgproto >> ../prototype.tmp
+find  ${PP}Bcfg2/Server/ ! -name "*.pyc"  | pkgproto >> ../prototype.tmp
+sed "s/`id | sed 's/uid=[0-9]*(\(.*\)) gid=[0-9]*(\(.*\))/\1 \2/'`/bin bin/" ../prototype.tmp > ../prototype.bcfg2-server
+
+rm ../prototype.tmp
