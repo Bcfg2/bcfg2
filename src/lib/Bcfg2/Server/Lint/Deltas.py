a8421cae0d2864e8bb9010656351f37d2db941c4
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2012-04-20 11:20:47 -0400

    make bcfg2-lint warn about .cat and .diff files

---
+++ src/lib/Bcfg2/Server/Lint/Deltas.py
@@ -0,0 +1,20 @@
+import Bcfg2.Server.Lint
+
+class Deltas(Bcfg2.Server.Lint.ServerPlugin):
+    """ Warn about usage of .cat and .diff files """
+
+    def Run(self):
+        """ run plugin """
+        if 'Cfg' in self.core.plugins:
+            cfg = self.core.plugins['Cfg']
+            for basename, entry in list(cfg.entries.items()):
+                self.check_entry(basename, entry)
+
+    def check_entry(self, basename, entry):
+        for fname in list(entry.entries.keys()):
+            match = entry.specific.delta_reg.match(fname)
+            if match:
+                self.LintError("%s-file-used" % match.group('delta'),
+                               "%s file used on %s: %s" % (match.group('delta'),
+                                                           basename,
+                                                           fname))
--- src/lib/Server/Lint/__init__.py
+++ src/lib/Server/Lint/__init__.py
@@ -8,7 +8,8 @@ __all__ = ['Bundles',
            'Pkgmgr',
            'RequiredAttrs',
            'Validate',
-           'Genshi']
+           'Genshi',
+           'Deltas']
 
 import logging
 import os
@@ -119,7 +120,9 @@ class ErrorHandler (object):
                "merge-probes":"warning",
                "input-output-error":"error",
                "genshi-syntax-error":"error",
-               "pattern-fails-to-initialize":"error"}
+               "pattern-fails-to-initialize":"error",
+               "cat-file-used":"warning",
+               "diff-file-used":"warning"}
 
     def __init__(self, config=None):
         self.errors = 0
