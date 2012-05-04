9aa10a0f2889a398ae9c7d074442b216bf9d8b55
Sol Jerome <sol.jerome@gmail.com>
2012-05-04 17:25:38 -0500

    reports: Remove unused import
    Signed-off-by: Sol Jerome <sol.jerome@gmail.com>

--- src/lib/Bcfg2/Server/Reports/reports/templatetags/syntax_coloring.py
+++ src/lib/Bcfg2/Server/Reports/reports/templatetags/syntax_coloring.py
@@ -1,6 +1,6 @@
 import sys
 from django import template
-from django.utils.encoding import smart_unicode, smart_str
+from django.utils.encoding import smart_unicode
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 
