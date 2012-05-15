3c6f694ff2c062d242c92889af94cac1b0a60a92
Sol Jerome <sol.jerome@gmail.com>
2012-05-15 08:50:45 -0500

    Packages: Fix URLError handling (Reported by dikim33 on IRC)
    Signed-off-by: Sol Jerome <sol.jerome@gmail.com>

--- src/lib/Server/Plugins/Packages/Yum.py
+++ src/lib/Server/Plugins/Packages/Yum.py
@@ -11,7 +11,7 @@ import lxml.etree
 from UserDict import DictMixin
 from subprocess import Popen, PIPE, STDOUT
 import Bcfg2.Server.Plugin
-from Bcfg2.Bcfg2Py3k import StringIO, cPickle, HTTPError, ConfigParser, file
+from Bcfg2.Bcfg2Py3k import StringIO, cPickle, HTTPError, URLError, ConfigParser, file
 from Bcfg2.Server.Plugins.Packages.Collection import Collection
 from Bcfg2.Server.Plugins.Packages.Source import SourceInitError, Source, \
      fetch_url
@@ -563,6 +563,11 @@ class YumSource(Source):
             xdata = lxml.etree.XML(repomd)
         except ValueError:
             self.logger.error("Packages: Bad url string %s" % rmdurl)
+            return []
+        except URLError:
+            err = sys.exc_info()[1]
+            self.logger.error("Packages: Failed to fetch url %s. %s" %
+                              (rmdurl, err))
             return []
         except HTTPError:
             err = sys.exc_info()[1]
