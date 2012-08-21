bfa749b055983a6a41961ac1f49f1a8284aebc6d
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2012-08-21 13:48:45 -0400

    fixed install of optional deps

--- testsuite/before_install.sh
+++ testsuite/before_install.sh
@@ -2,7 +2,7 @@
 
 # before_install script for Travis-CI
 
-apt-get update -qq
+sudo apt-get update -qq
 if [[ "$WITH_OPTIONAL_DEPS" == "yes" ]]; then
-    apt-get install -qq python-selinux python-pylibacl
+    sudo apt-get install -qq python-selinux python-pylibacl
 fi
