b08bf90dff32de73521c74a68968ef6525b72360
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2012-08-21 17:13:14 -0400

    install swig so m2crypto installation can succeed

--- testsuite/before_install.sh
+++ testsuite/before_install.sh
@@ -3,6 +3,7 @@
 # before_install script for Travis-CI
 
 sudo apt-get update -qq
+sudo apt-get install -qq swig
 if [[ "$WITH_OPTIONAL_DEPS" == "yes" ]]; then
     sudo apt-get install -qq python-selinux python-pylibacl
 fi
