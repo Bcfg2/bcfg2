e88ef5ad2db84aa2664330096881a4d627d62b8c
Sol Jerome <sol.jerome@gmail.com>
2012-10-15 13:50:23 -0500

    POSIXCompat: Revive compatibility plugin for POSIX
    This plugin adds back the perms attribute for older client
    compatibility. Can be removed in a future release.
    
    Signed-off-by: Sol Jerome <sol.jerome@gmail.com>

---
+++ src/lib/Bcfg2/Server/Plugins/POSIXCompat.py
@@ -0,0 +1,22 @@
+"""This plugin provides a compatibility layer which turns new-style
+POSIX entries into old-style entries.
+"""
+
+import Bcfg2.Server.Plugin
+
+
+class POSIXCompat(Bcfg2.Server.Plugin.Plugin,
+                  Bcfg2.Server.Plugin.GoalValidator):
+    """POSIXCompat is a goal validator plugin for POSIX entries."""
+    name = 'POSIXCompat'
+
+    def __init__(self, core, datastore):
+        Bcfg2.Server.Plugin.Plugin.__init__(self, core, datastore)
+        Bcfg2.Server.Plugin.GoalValidator.__init__(self)
+
+    def validate_goals(self, metadata, goals):
+        """Verify that we are generating correct old POSIX entries."""
+        for goal in goals:
+            for entry in goal.getchildren():
+                if entry.tag == 'Path' and 'mode' in entry.keys():
+                    entry.set('perms', entry.get('mode'))
