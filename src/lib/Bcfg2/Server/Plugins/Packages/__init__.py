6fa3424007269b17d0b8bf8bf2ff770cfd1b2e56
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2012-10-16 10:49:36 -0400

    Packages: import error we catch

--- src/lib/Bcfg2/Server/Plugins/Packages/__init__.py
+++ src/lib/Bcfg2/Server/Plugins/Packages/__init__.py
@@ -9,7 +9,7 @@ import shutil
 import lxml.etree
 import Bcfg2.Logger
 import Bcfg2.Server.Plugin
-from Bcfg2.Compat import ConfigParser, urlopen
+from Bcfg2.Compat import ConfigParser, urlopen, HTTPError
 from Bcfg2.Server.Plugins.Packages.Collection import Collection, \
     get_collection_class
 from Bcfg2.Server.Plugins.Packages.PackagesSources import PackagesSources
