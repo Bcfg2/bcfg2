12684bada59c9ddc08165dad757682514b54634c
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2012-11-15 17:03:23 -0500

    Git: Added ability to update to a specific tree-ish

--- src/lib/Bcfg2/Server/Plugins/Git.py
+++ src/lib/Bcfg2/Server/Plugins/Git.py
@@ -52,17 +52,48 @@ class Git(Bcfg2.Server.Plugin.Version):
             self.logger.error(msg)
             raise Bcfg2.Server.Plugin.PluginExecutionError(msg)
 
-    def Update(self):
+    def Update(self, ref=None):
         """ Git.Update() => True|False
         Update the working copy against the upstream repository
         """
+        self.logger.info("Git: Git.Update(ref='%s')" % ref)
+        self.debug_log("Git: Performing garbage collection on repo at %s" %
+                       self.vcs_root)
         try:
-            self.repo.git.pull("--rebase")
-            self.logger.info("Git repo at %s updated to %s" %
-                             (self.vcs_root, self.get_revision()))
-            return True
-        except:  # pylint: disable=W0702
-            err = sys.exc_info()[1]
-            msg = "Failed to pull from git repository: %s" % err
-            self.logger.error(msg)
-            raise Bcfg2.Server.Plugin.PluginExecutionError(msg)
+            self.repo.git.gc('--auto')
+        except git.GitCommandError:
+            self.logger.warning("Git: Failed to perform garbage collection: %s"
+                                % sys.exc_info()[1])
+
+        if ref:
+            self.debug_log("Git: Checking out %s" % ref)
+            try:
+                self.repo.git.checkout('-f', ref)
+            except git.GitCommandError:
+                err = sys.exc_info()[1]
+                msg = "Git: Failed to checkout %s: %s" % (ref, err)
+                self.logger.error(msg)
+                raise Bcfg2.Server.Plugin.PluginExecutionError(msg)
+
+        # determine if we should try to pull to get the latest commit
+        # on this head
+        tracking = None
+        if not self.repo.head.is_detached:
+            self.debug_log("Git: Determining if %s is a tracking branch" %
+                           self.repo.head.ref.name)
+            tracking = self.repo.head.ref.tracking_branch()
+
+        if tracking is not None:
+            self.debug_log("Git: %s is a tracking branch, pulling from %s" %
+                           (self.repo.head.ref.name, tracking))
+            try:
+                self.repo.git.pull("--rebase")
+            except:  # pylint: disable=W0702
+                err = sys.exc_info()[1]
+                msg = "Git: Failed to pull from upstream: %s" % err
+                self.logger.error(msg)
+                raise Bcfg2.Server.Plugin.PluginExecutionError(msg)
+
+        self.logger.info("Git: Repo at %s updated to %s" %
+                         (self.vcs_root, self.get_revision()))
+        return True
