e5072626bc50865ca95e64fa0ca78262655d2164
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2013-02-06 15:45:30 -0500

    fixed loading FAM in core

--- src/lib/Bcfg2/Server/Core.py
+++ src/lib/Bcfg2/Server/Core.py
@@ -134,7 +134,7 @@ class BaseCore(object):
             #: The :class:`Bcfg2.Server.FileMonitor.FileMonitor`
             #: object used by the core to monitor for Bcfg2 data
             #: changes.
-            self.fam = Bcfg2.Server.FileMonitor.get_fam(**famargs)
+            self.fam = Bcfg2.Server.FileMonitor.load_fam(**famargs)
         except IOError:
             msg = "Failed to instantiate fam driver %s" % \
                 self.setup['filemonitor']
