Bcfg2/bcfg2

commit 6c996f42c53a36fc0406f836d64b8c1bec6f4bcc

testsuite: fixed issues found by latest version of pylint

src/lib/Bcfg2/Server/Plugin/helpers.py
@@ -41,15 +41,15 @@ del DEFAULT_FILE_METADATA['configfile']
 LOGGER = logging.getLogger(__name__)
 
 #: a compiled regular expression for parsing info and :info files
-INFO_REGEX = re.compile('owner:(\s)*(?P<owner>\S+)|' +
-                        'group:(\s)*(?P<group>\S+)|' +
-                        'mode:(\s)*(?P<mode>\w+)|' +
-                        'secontext:(\s)*(?P<secontext>\S+)|' +
-                        'paranoid:(\s)*(?P<paranoid>\S+)|' +
-                        'sensitive:(\s)*(?P<sensitive>\S+)|' +
-                        'encoding:(\s)*(?P<encoding>\S+)|' +
-                        'important:(\s)*(?P<important>\S+)|' +
-                        'mtime:(\s)*(?P<mtime>\w+)|')
+INFO_REGEX = re.compile(r'owner:(\s)*(?P<owner>\S+)|' +
+                        r'group:(\s)*(?P<group>\S+)|' +
+                        r'mode:(\s)*(?P<mode>\w+)|' +
+                        r'secontext:(\s)*(?P<secontext>\S+)|' +
+                        r'paranoid:(\s)*(?P<paranoid>\S+)|' +
+                        r'sensitive:(\s)*(?P<sensitive>\S+)|' +
+                        r'encoding:(\s)*(?P<encoding>\S+)|' +
+                        r'important:(\s)*(?P<important>\S+)|' +
+                        r'mtime:(\s)*(?P<mtime>\w+)|')
 
 
 def bind_info(entry, metadata, infoxml=None, default=DEFAULT_FILE_METADATA):
@@ -873,7 +873,7 @@ class XMLDirectoryBacked(DirectoryBacked):
 
     #: Only track and include files whose names (not paths) match this
     #: compiled regex.
-    patterns = re.compile('^.*\.xml$')
+    patterns = re.compile(r'^.*\.xml$')
 
     #: The type of child objects to create for files contained within
     #: the directory that is tracked.  Default is
@@ -1142,7 +1142,7 @@ class EntrySet(Debuggable):
     #: file is encountered that does not match the ``basename``
     #: argument passed to the constructor or ``ignore``, then a
     #: warning will be produced.
-    ignore = re.compile("^(\.#.*|.*~|\\..*\\.(sw[px])|.*\\.genshi_include)$")
+    ignore = re.compile(r'^(\.#.*|.*~|\\..*\\.(sw[px])|.*\\.genshi_include)$')
 
     # The ``basename`` argument passed to the constructor will be
     #: processed as a string that contains a regular expression (i.e.,
@@ -1205,8 +1205,8 @@ class EntrySet(Debuggable):
             base_pat = basename
         else:
             base_pat = re.escape(basename)
-        pattern = '(.*/)?%s(\.((H_(?P<hostname>\S+))|' % base_pat
-        pattern += '(G(?P<prio>\d+)_(?P<group>\S+))))?$'
+        pattern = r'(.*/)?%s(\.((H_(?P<hostname>\S+))|' % base_pat
+        pattern += r'(G(?P<prio>\d+)_(?P<group>\S+))))?$'
 
         #: ``specific`` is a regular expression that is used to
         #: determine the specificity of a file in this entry set.  It

src/lib/Bcfg2/Server/Plugins/Cfg/__init__.py
@@ -111,12 +111,12 @@ class CfgBaseFileMatcher(Bcfg2.Server.Plugin.SpecificData,
         components = ['^(?P<basename>%s)' % '|'.join(re.escape(b)
                                                      for b in basenames)]
         if cls.__specific__:
-            components.append('(|\\.H_(?P<hostname>\S+?)|' +
-                              '\.G(?P<prio>\d+)_(?P<group>\S+?))')
+            components.append(r'(|\\.H_(?P<hostname>\S+?)|' +
+                              r'\.G(?P<prio>\d+)_(?P<group>\S+?))')
         if cls.__extensions__:
-            components.append('\\.(?P<extension>%s)' %
-                              '|'.join(cls.__extensions__))
-        components.append('$')
+            components.append(r'\\.(?P<extension>%s)' %
+                              r'|'.join(cls.__extensions__))
+        components.append(r'$')
         return re.compile("".join(components))
 
     @classmethod

src/lib/Bcfg2/Server/Plugins/Probes.py
@@ -111,15 +111,15 @@ class ProbeData(str):
 
 class ProbeSet(Bcfg2.Server.Plugin.EntrySet):
     """ Handle universal and group- and host-specific probe files """
-    ignore = re.compile("^(\.#.*|.*~|\\..*\\.(tmp|sw[px])|probed\\.xml)$")
+    ignore = re.compile(r'^(\.#.*|.*~|\\..*\\.(tmp|sw[px])|probed\\.xml)$')
     probename = \
-        re.compile("(.*/)?(?P<basename>\S+?)(\.(?P<mode>(?:G\d\d)|H)_\S+)?$")
-    bangline = re.compile('^#!\s*(?P<interpreter>.*)$')
+        re.compile(r'(.*/)?(?P<basename>\S+?)(\.(?P<mode>(?:G\d\d)|H)_\S+)?$')
+    bangline = re.compile(r'^#!\s*(?P<interpreter>.*)$')
     basename_is_regex = True
 
     def __init__(self, path, fam, encoding, plugin_name):
         self.plugin_name = plugin_name
-        Bcfg2.Server.Plugin.EntrySet.__init__(self, '[0-9A-Za-z_\-]+', path,
+        Bcfg2.Server.Plugin.EntrySet.__init__(self, r'[0-9A-Za-z_\-]+', path,
                                               Bcfg2.Server.Plugin.SpecificData,
                                               encoding)
         fam.AddMonitor(path, self)

src/lib/Bcfg2/Client/__init__.py
@@ -1,11 +1,11 @@
 """This contains all Bcfg2 Client modules"""
 
-__all__ = ["Frame", "Tools", "XML", "Client"]
-
 import os
 import sys
 import select
-from Bcfg2.Compat import input  # pylint: disable=W0622
+from Bcfg2.Compat import input, walk_packages  # pylint: disable=W0622
+
+__all__ = [m[1] for m in walk_packages(path=__path__)]
 
 
 def prompt(msg):

src/lib/Bcfg2/Server/Admin/__init__.py
@@ -1,29 +1,14 @@
 """ Base classes for admin modes """
 
-__all__ = ['Backup',
-           'Bundle',
-           'Client',
-           'Compare',
-           'Group',
-           'Init',
-           'Minestruct',
-           'Perf',
-           'Pull',
-           'Query',
-           'Reports',
-           'Snapshots',
-           'Syncdb',
-           'Tidy',
-           'Viz',
-           'Xcmd']
-
 import re
 import sys
 import logging
 import lxml.etree
 import Bcfg2.Server.Core
 import Bcfg2.Options
-from Bcfg2.Compat import ConfigParser
+from Bcfg2.Compat import ConfigParser, walk_packages
+
+__all__ = [m[1] for m in walk_packages(path=__path__)]
 
 
 class Mode(object):

src/lib/Bcfg2/Server/Plugins/__init__.py
@@ -1,30 +1,5 @@
 """Imports for Bcfg2.Server.Plugins."""
 
-__all__ = ['Account',
-           'Base',
-           'Bundler',
-           'Bzr',
-           'Cfg',
-           'Cvs',
-           'Darcs',
-           'Decisions',
-           'Fossil',
-           'Git',
-           'GroupPatterns',
-           'Hg',
-           'Hostbase',
-           'Metadata',
-           'NagiosGen',
-           'Ohai',
-           'Packages',
-           'Properties',
-           'Probes',
-           'Pkgmgr',
-           'Rules',
-           'SSHbase',
-           'Snapshots',
-           'Statistics',
-           'Svn',
-           'TCheetah',
-           'Trigger',
-           'TGenshi']
+from Bcfg2.Compat import walk_packages
+
+__all__ = [m[1] for m in walk_packages(path=__path__)]

src/lib/Bcfg2/Server/__init__.py
@@ -1,10 +1,9 @@
 """This is the set of modules for Bcfg2.Server."""
 
 import lxml.etree
+from Bcfg2.Compat import walk_packages
 
-__all__ = ["Admin", "Core", "FileMonitor", "Plugin", "Plugins",
-           "Hostbase", "Reports", "Snapshots", "XMLParser",
-           "XI", "XI_NAMESPACE"]
+__all__ = [m[1] for m in walk_packages(path=__path__)]
 
 XI = 'http://www.w3.org/2001/XInclude'
 XI_NAMESPACE = '{%s}' % XI

src/lib/Bcfg2/__init__.py
@@ -1,3 +1,4 @@
 """Base modules definition."""
 
-__all__ = ['Server', 'Client', 'Logger', 'Options', 'Proxy', 'Statistics']
+from Bcfg2.Compat import walk_packages
+__all__ = [m[1] for m in walk_packages(path=__path__)]

commit 1ab5df5b1ed6b6082ba453677450bb1b177fcfc0

fixed regex errors introduced by 6c996f42c53a36fc0406f836d64b8c1bec6f4bcc

src/lib/Bcfg2/Server/Plugin/helpers.py
@@ -1152,7 +1152,7 @@ class EntrySet(Debuggable):
     #: file is encountered that does not match the ``basename``
     #: argument passed to the constructor or ``ignore``, then a
     #: warning will be produced.
-    ignore = re.compile(r'^(\.#.*|.*~|\\..*\\.(sw[px])|.*\\.genshi_include)$')
+    ignore = re.compile(r'^(\.#.*|.*~|\..*\.(sw[px])|.*\.genshi_include)$')
 
     # The ``basename`` argument passed to the constructor will be
     #: processed as a string that contains a regular expression (i.e.,
@@ -1215,8 +1215,8 @@ class EntrySet(Debuggable):
             base_pat = basename
         else:
             base_pat = re.escape(basename)
-        pattern = r'(.*/)?%s(\.((H_(?P<hostname>\S+))|' % base_pat
-        pattern += r'(G(?P<prio>\d+)_(?P<group>\S+))))?$'
+        pattern = r'(.*/)?' + base_pat + \
+            r'(\.((H_(?P<hostname>\S+))|(G(?P<prio>\d+)_(?P<group>\S+))))?$'
 
         #: ``specific`` is a regular expression that is used to
         #: determine the specificity of a file in this entry set.  It

src/lib/Bcfg2/Server/Plugins/Cfg/__init__.py
@@ -111,10 +111,10 @@ class CfgBaseFileMatcher(Bcfg2.Server.Plugin.SpecificData,
         components = ['^(?P<basename>%s)' % '|'.join(re.escape(b)
                                                      for b in basenames)]
         if cls.__specific__:
-            components.append(r'(|\\.H_(?P<hostname>\S+?)|' +
+            components.append(r'(|\.H_(?P<hostname>\S+?)|' +
                               r'\.G(?P<prio>\d+)_(?P<group>\S+?))')
         if cls.__extensions__:
-            components.append(r'\\.(?P<extension>%s)' %
+            components.append(r'\.(?P<extension>%s)' %
                               r'|'.join(cls.__extensions__))
         components.append(r'$')
         return re.compile("".join(components))

src/lib/Bcfg2/Server/Plugins/Probes.py
@@ -111,7 +111,7 @@ class ProbeData(str):
 
 class ProbeSet(Bcfg2.Server.Plugin.EntrySet):
     """ Handle universal and group- and host-specific probe files """
-    ignore = re.compile(r'^(\.#.*|.*~|\\..*\\.(tmp|sw[px])|probed\\.xml)$')
+    ignore = re.compile(r'^(\.#.*|.*~|\..*\.(tmp|sw[px])|probed\.xml)$')
     probename = \
         re.compile(r'(.*/)?(?P<basename>\S+?)(\.(?P<mode>(?:G\d\d)|H)_\S+)?$')
     bangline = re.compile(r'^#!\s*(?P<interpreter>.*)$')

testsuite/Testsrc/Testlib/TestServer/TestPlugin/Testhelpers.py
@@ -1477,7 +1477,8 @@ class TestEntrySet(TestDebuggable):
                                                                   bogus)))
 
             for ignore in self.ignore:
-                self.assertTrue(eset.ignore.match(ignore))
+                self.assertTrue(eset.ignore.match(ignore),
+                                "%s should be ignored but wasn't" % ignore)
 
             self.assertFalse(eset.ignore.match(basename))
             self.assertFalse(eset.ignore.match(basename + ".G20_foo"))