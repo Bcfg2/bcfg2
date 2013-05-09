3f6c8ab635137087913dd4973143d714d8f79ed8
Hugh Brown <hbrown@wgen.net>
2013-05-09 18:14:53 -0400

    ./Server/Hostbase/urls.py: E128 continuation line under-indented for visual indent

--- src/lib/Bcfg2/Server/Hostbase/urls.py
+++ src/lib/Bcfg2/Server/Hostbase/urls.py
@@ -8,20 +8,20 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+                       # to INSTALLED_APPS to enable admin documentation:
+                      (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+                       # Uncomment the next line to enable the admin:
+                      (r'^admin/', include(admin.site.urls)),
 
-    (r'^$', direct_to_template, {'template': 'index.html'}, 'index'),
-    (r'^hostbase/', include('hostbase.urls')),
-    (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
-    (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'})
+                      (r'^$', direct_to_template, {'template': 'index.html'}, 'index'),
+                      (r'^hostbase/', include('hostbase.urls')),
+                      (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+                      (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'})
 )
 
 if settings.SERVE_MEDIA:
     urlpatterns += patterns('',
-        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-         dict(document_root=settings.MEDIA_ROOT)),)
+                           (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+                            dict(document_root=settings.MEDIA_ROOT)),)
