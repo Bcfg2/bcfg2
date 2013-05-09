4d4203ccaef4db821ab3a6dc0e3a3fd0a563e406
Hugh Brown <hbrown@wgen.net>
2013-05-09 18:14:35 -0400

    ./Client/Tools/APT.py: E127 continuation line over-indented for visual indent

--- src/lib/Bcfg2/Client/Tools/APT.py
+++ src/lib/Bcfg2/Client/Tools/APT.py
@@ -47,16 +47,16 @@ class APT(Bcfg2.Client.Tools.Tool):
                         if entry.tag == 'Path' and
                         entry.get('type') == 'ignore']
         self.__important__ = self.__important__ + \
-                             ["%s/cache/debconf/config.dat" % self.var_path,
-                              "%s/cache/debconf/templates.dat" % self.var_path,
-                              '/etc/passwd', '/etc/group',
-                              '%s/apt/apt.conf' % self.etc_path,
-                              '%s/dpkg/dpkg.cfg' % self.etc_path] + \
-                             [entry.get('name') for struct in config for entry in struct
-                              if entry.tag == 'Path' and
-                              entry.get('name').startswith('%s/apt/sources.list' % self.etc_path)]
+            ["%s/cache/debconf/config.dat" % self.var_path,
+            "%s/cache/debconf/templates.dat" % self.var_path,
+            '/etc/passwd', '/etc/group',
+            '%s/apt/apt.conf' % self.etc_path,
+            '%s/dpkg/dpkg.cfg' % self.etc_path] + \
+            [entry.get('name') for struct in config for entry in struct
+            if entry.tag == 'Path' and
+            entry.get('name').startswith('%s/apt/sources.list' % self.etc_path)]
         self.nonexistent = [entry.get('name') for struct in config for entry in struct
-                              if entry.tag == 'Path' and entry.get('type') == 'nonexistent']
+                            if entry.tag == 'Path' and entry.get('type') == 'nonexistent']
         os.environ["DEBIAN_FRONTEND"] = 'noninteractive'
         self.actions = {}
         if self.setup['kevlar'] and not self.setup['dryrun']:
@@ -87,7 +87,7 @@ class APT(Bcfg2.Client.Tools.Tool):
                       if p.isInstalled and p.name not in packages]
         return [Bcfg2.Client.XML.Element('Package', name=name,
                                          type='deb', version=version)
-                                         for (name, version) in extras]
+                for (name, version) in extras]
 
     def VerifyDebsums(self, entry, modlist):
         output = \
