a96859fd61a301fe0620284f7d1881d9657ae628
Hugh Brown <hbrown@wgen.net>
2013-05-09 18:09:37 -0400

    ./Server/Plugins/Account.py: E502 the backslash is redundant between brackets

--- src/lib/Bcfg2/Server/Plugins/Account.py
+++ src/lib/Bcfg2/Server/Plugins/Account.py
@@ -30,7 +30,7 @@ class Account(Bcfg2.Server.Plugin.Plugin,
             self.repository = Bcfg2.Server.Plugin.DirectoryBacked(self.data,
                                                                   self.core.fam)
         except:
-            self.logger.error("Failed to load repos: %s, %s" % \
+            self.logger.error("Failed to load repos: %s, %s" %
                               (self.data, "%s/ssh" % (self.data)))
             raise Bcfg2.Server.Plugin.PluginInitError
 
@@ -42,21 +42,21 @@ class Account(Bcfg2.Server.Plugin.Plugin,
         perms = {'owner': 'root',
                  'group': 'root',
                  'mode': '0644'}
-        [entry.attrib.__setitem__(key, value) for (key, value) in \
+        [entry.attrib.__setitem__(key, value) for (key, value) in
          list(perms.items())]
 
     def gen_limits_cb(self, entry, metadata):
         """Build limits entries based on current ACLs."""
         entry.text = self.repository.entries["static.limits.conf"].data
         superusers = self.repository.entries["superusers"].data.split()
-        useraccess = [line.split(':') for line in \
+        useraccess = [line.split(':') for line in
                       self.repository.entries["useraccess"].data.split()]
-        users = [user for (user, host) in \
+        users = [user for (user, host) in
                  useraccess if host == metadata.hostname.split('.')[0]]
         perms = {'owner': 'root',
                  'group': 'root',
                  'mode': '0600'}
-        [entry.attrib.__setitem__(key, value) for (key, value) in \
+        [entry.attrib.__setitem__(key, value) for (key, value) in
          list(perms.items())]
         entry.text += "".join(["%s hard maxlogins 1024\n" % uname for uname in superusers + users])
         if "*" not in users:
@@ -66,37 +66,37 @@ class Account(Bcfg2.Server.Plugin.Plugin,
         """Build root authorized keys file based on current ACLs."""
         superusers = self.repository.entries['superusers'].data.split()
         try:
-            rootlike = [line.split(':', 1) for line in \
+            rootlike = [line.split(':', 1) for line in
                         self.repository.entries['rootlike'].data.split()]
-            superusers += [user for (user, host) in rootlike \
+            superusers += [user for (user, host) in rootlike
                            if host == metadata.hostname.split('.')[0]]
         except:
             pass
         rdata = self.repository.entries
-        entry.text = "".join([rdata["%s.key" % user].data for user \
-                              in superusers if \
+        entry.text = "".join([rdata["%s.key" % user].data for user
+                              in superusers if
                               ("%s.key" % user) in rdata])
         perms = {'owner': 'root',
                  'group': 'root',
                  'mode': '0600'}
-        [entry.attrib.__setitem__(key, value) for (key, value) \
+        [entry.attrib.__setitem__(key, value) for (key, value)
          in list(perms.items())]
 
     def gen_sudoers(self, entry, metadata):
         """Build root authorized keys file based on current ACLs."""
         superusers = self.repository.entries['superusers'].data.split()
         try:
-            rootlike = [line.split(':', 1) for line in \
+            rootlike = [line.split(':', 1) for line in
                         self.repository.entries['rootlike'].data.split()]
-            superusers += [user for (user, host) in rootlike \
+            superusers += [user for (user, host) in rootlike
                            if host == metadata.hostname.split('.')[0]]
         except:
             pass
         entry.text = self.repository.entries['static.sudoers'].data
-        entry.text += "".join(["%s ALL=(ALL) ALL\n" % uname \
+        entry.text += "".join(["%s ALL=(ALL) ALL\n" % uname
                                for uname in superusers])
         perms = {'owner': 'root',
                  'group': 'root',
                  'mode': '0440'}
-        [entry.attrib.__setitem__(key, value) for (key, value) \
+        [entry.attrib.__setitem__(key, value) for (key, value)
          in list(perms.items())]
