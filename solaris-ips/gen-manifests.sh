e9603a3456bbf252cb40c64688088a4de2ec5b62
Dan Foster <dan.foster@bristol.ac.uk>
2013-05-30 14:24:00 +0000

    Solaris 11 IPS Package support

---
+++ solaris-ips/gen-manifests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/sh
+
+#bcfg2
+cat MANIFEST.bcfg2.header > MANIFEST.bcfg2
+pkgsend generate build | grep man[15] >> MANIFEST.bcfg2
+pkgsend generate build | grep  Bcfg2/[^/]*.py$ >> MANIFEST.bcfg2
+pkgsend generate build | grep  Bcfg2/Client/.*.py$ >> MANIFEST.bcfg2
+
+#bcfg2-server
+cat MANIFEST.bcfg2-server.header > MANIFEST.bcfg2-server
+pkgsend generate build | grep man[8] >> MANIFEST.bcfg2-server
+pkgsend generate build | grep share/bcfg2 >> MANIFEST.bcfg2-server
+pkgsend generate build | grep bin/bcfg2- >> MANIFEST.bcfg2-server
+pkgsend generate build | grep  Bcfg2/[^/]*.py$ >>   MANIFEST.bcfg2-server
+pkgsend generate build | grep  Bcfg2/Server/.*.py$ >> MANIFEST.bcfg2-server
+
