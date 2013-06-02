5ef9138ae6f8c27ae41e6c263cd7e4d64d743c22
Dan Foster <dan.foster@bristol.ac.uk>
2013-06-02 11:10:11 +0000

    Remove accidental extra client files from server package

--- solaris-ips/gen-manifests.sh
+++ solaris-ips/gen-manifests.sh
@@ -11,6 +11,5 @@ cat MANIFEST.bcfg2-server.header > MANIFEST.bcfg2-server
 pkgsend generate build | grep man[8] >> MANIFEST.bcfg2-server
 pkgsend generate build | grep share/bcfg2 >> MANIFEST.bcfg2-server
 pkgsend generate build | grep bin/bcfg2- >> MANIFEST.bcfg2-server
-pkgsend generate build | grep  Bcfg2/[^/]*.py$ >>   MANIFEST.bcfg2-server
 pkgsend generate build | grep  Bcfg2/Server/.*.py$ >> MANIFEST.bcfg2-server
 
