57aa79fa5acf676694e6d653d9d04753383723fb
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2013-09-23 12:34:48 -0400

    Probes: set _use_db property correctly

--- src/lib/Bcfg2/Server/Plugins/Probes.py
+++ src/lib/Bcfg2/Server/Plugins/Probes.py
@@ -121,10 +121,13 @@ class DBProbeStore(ProbeStore, Bcfg2.Server.Plugin.DatabaseBacked):
     create = False
 
     def __init__(self, core, datadir):
-        self._use_db = True
         Bcfg2.Server.Plugin.DatabaseBacked.__init__(self, core,
                                                     os.path.dirname(datadir))
         ProbeStore.__init__(self, core, datadir)
+
+    @property
+    def _use_db(self):
+        return True
 
     def _load_groups(self, hostname):
         Bcfg2.Server.Cache.expire("Probes", "probegroups", hostname)
