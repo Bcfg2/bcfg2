7f6c10db41c22b3924539aae19164a9ab9a80468
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2013-10-03 16:24:05 -0400

    Metadata: import any() from Compat

--- src/lib/Bcfg2/Server/Plugins/Metadata.py
+++ src/lib/Bcfg2/Server/Plugins/Metadata.py
@@ -16,7 +16,9 @@ import Bcfg2.Server.Lint
 import Bcfg2.Server.Plugin
 import Bcfg2.Server.FileMonitor
 from Bcfg2.Utils import locked
-from Bcfg2.Compat import MutableMapping, all, wraps  # pylint: disable=W0622
+# pylint: disable=W0622
+from Bcfg2.Compat import MutableMapping, all, any, wraps
+# pylint: enable=W0622
 from Bcfg2.version import Bcfg2VersionInfo
 
 try:
