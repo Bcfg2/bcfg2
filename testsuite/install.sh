1d27fdadac37eb95d56f98cf7bdd1721a339df81
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2013-10-28 09:11:36 -0400

    testsuite: install django 1.4.8 when testing py 2.5

--- testsuite/install.sh
+++ testsuite/install.sh
@@ -10,7 +10,9 @@ if [[ "$WITH_OPTIONAL_DEPS" == "yes" ]]; then
     pip install --use-mirrors genshi PyYAML pyinotify boto
     if [[ $PYVER == "2.5" ]]; then
         # markdown 2.2+ doesn't work on py2.5
-        pip install --use-mirrors simplejson 'markdown<2.2'
+        pip install --use-mirrors simplejson 'markdown<2.2' 'django<1.4.9'
+    else
+        pip install 'django<1.5'
     fi
     if [[ ${PYVER:0:1} == "2" ]]; then
         # django supports py3k, but South doesn't, and the django bits
