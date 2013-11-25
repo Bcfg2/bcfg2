a02509650edd374b60ef178d217cda7faab24141
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2013-11-25 07:54:27 -0500

    testsuite: removed py2.5 from travis tests, no longer supported
    http://about.travis-ci.org/blog/2013-11-18-upcoming-build-environment-updates/

--- testsuite/install.sh
+++ testsuite/install.sh
@@ -7,22 +7,10 @@ pip install -r testsuite/requirements.txt --use-mirrors
 PYVER=$(python -c 'import sys;print(".".join(str(v) for v in sys.version_info[0:2]))')
 
 if [[ "$WITH_OPTIONAL_DEPS" == "yes" ]]; then
-    pip install --use-mirrors genshi PyYAML pyinotify boto
-    if [[ $PYVER == "2.5" ]]; then
-        # markdown 2.2+ doesn't work on py2.5
-        pip install --use-mirrors simplejson 'markdown<2.2' 'django<1.4.9'
-    else
-        pip install 'django<1.5'
-    fi
+    pip install --use-mirrors genshi PyYAML pyinotify boto 'django<1.5'
     if [[ ${PYVER:0:1} == "2" ]]; then
         # django supports py3k, but South doesn't, and the django bits
         # in bcfg2 require South
         pip install cheetah 'South<0.8' M2Crypto
-    fi
-else
-    # python < 2.6 requires M2Crypto for SSL communication, not just
-    # for encryption support
-    if [[ $PYVER == "2.5" || $PYVER == "2.4" ]]; then
-        pip install --use-mirrors M2crypto
     fi
 fi
