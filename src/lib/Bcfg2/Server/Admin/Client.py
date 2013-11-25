a87c59d6f23b4c4fd4e38e380994f2193ca24588
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2013-11-25 10:48:43 -0500

    bcfg2-admin: Restored missing "bcfg2-admin client add ... attrib=val" functionality

--- src/lib/Bcfg2/Server/Admin/Client.py
+++ src/lib/Bcfg2/Server/Admin/Client.py
@@ -5,6 +5,18 @@ import Bcfg2.Server.Admin
 from Bcfg2.Server.Plugin import MetadataConsistencyError
 
 
+def get_attribs(args):
+    attr_d = {}
+    for i in args[2:]:
+        attr, val = i.split('=', 1)
+        if attr not in ['profile', 'uuid', 'password', 'floating', 'secure',
+                        'address', 'auth']:
+            print("Attribute %s unknown" % attr)
+            raise SystemExit(1)
+        attr_d[attr] = val
+    return attr_d
+
+
 class Client(Bcfg2.Server.Admin.MetadataCore):
     """ Create, delete, or list client entries """
     __usage__ = "[options] [add|del|list] [attr=val]"
@@ -16,14 +28,19 @@ class Client(Bcfg2.Server.Admin.MetadataCore):
                          "Usage: %s" % self.__usage__)
         if args[0] == 'add':
             try:
-                self.metadata.add_client(args[1])
+                self.metadata.add_client(args[1], get_attribs(args))
             except MetadataConsistencyError:
-                self.errExit("Error in adding client: %s" % sys.exc_info()[1])
+                self.errExit("Error adding client: %s" % sys.exc_info()[1])
+        elif args[0] in ['update', 'up']:
+            try:
+                self.metadata.update_client(args[1], get_attribs(args))
+            except MetadataConsistencyError:
+                self.errExit("Error updating client: %s" % sys.exc_info()[1])
         elif args[0] in ['delete', 'remove', 'del', 'rm']:
             try:
                 self.metadata.remove_client(args[1])
             except MetadataConsistencyError:
-                self.errExit("Error in deleting client: %s" %
+                self.errExit("Error deleting client: %s" %
                              sys.exc_info()[1])
         elif args[0] in ['list', 'ls']:
             for client in self.metadata.list_clients():
