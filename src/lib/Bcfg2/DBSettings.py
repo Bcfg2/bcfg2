36d38034fc4b9a37dd8b4009e392b00af91effda
Matt Kemp <matt@mattikus.com>
2014-06-12 17:32:30 -0500

    Fix admins and managers in django config
    This should eventually be a configurable.

--- src/lib/Bcfg2/DBSettings.py
+++ src/lib/Bcfg2/DBSettings.py
@@ -26,8 +26,8 @@ settings = dict(  # pylint: disable=C0103
     DEBUG=False,
     ALLOWED_HOSTS=['*'],
     MEDIA_URL='/site_media/',
-    MANAGERS=(('Root', 'root')),
-    ADMINS=(('Root', 'root')),
+    MANAGERS=(('Root', 'root'),),
+    ADMINS=(('Root', 'root'),),
     # Language code for this installation. All choices can be found
     # here:
     # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
