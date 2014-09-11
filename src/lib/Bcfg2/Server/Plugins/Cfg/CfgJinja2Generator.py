4cc4989ee2227e7527c639186dac3f0f0b2710fd
Gordon Messmer <gordon@dragonsdawn.net>
2014-09-11 14:02:09 -0700

    Update CfgJinja2Generator.py to more closely match git master code. The first version matched Bcfg2-1.3.4.

--- src/lib/Bcfg2/Server/Plugins/Cfg/CfgJinja2Generator.py
+++ src/lib/Bcfg2/Server/Plugins/Cfg/CfgJinja2Generator.py
@@ -2,14 +2,28 @@
 <http://jinja.pocoo.org/>`_ templating system to generate
 :ref:`server-plugins-generators-cfg` files. """
 
-from Bcfg2.Server.Plugin import PluginExecutionError
-from Bcfg2.Server.Plugins.Cfg import CfgGenerator, SETUP
+import Bcfg2.Options
+from Bcfg2.Server.Plugin import PluginExecutionError, \
+    DefaultTemplateDataProvider, get_template_data
+from Bcfg2.Server.Plugins.Cfg import CfgGenerator
 
 try:
     from jinja2 import Template
     HAS_JINJA2 = True
 except ImportError:
     HAS_JINJA2 = False
+
+
+class DefaultJinja2DataProvider(DefaultTemplateDataProvider):
+    """ Template data provider for Jinja2 templates. Jinja2 and
+    Genshi currently differ over the value of the ``path`` variable,
+    which is why this is necessary. """
+
+    def get_template_data(self, entry, metadata, template):
+        rv = DefaultTemplateDataProvider.get_template_data(self, entry,
+                                                           metadata, template)
+        rv['path'] = rv['name']
+        return rv
 
 
 class CfgJinja2Generator(CfgGenerator):
@@ -24,15 +38,15 @@ class CfgJinja2Generator(CfgGenerator):
     #: .crypt.jinja2 files
     __priority__ = 50
 
-    def __init__(self, fname, spec, encoding):
-        CfgGenerator.__init__(self, fname, spec, encoding)
+    def __init__(self, fname, spec):
+        CfgGenerator.__init__(self, fname, spec)
         if not HAS_JINJA2:
             raise PluginExecutionError("Jinja2 is not available")
     __init__.__doc__ = CfgGenerator.__init__.__doc__
 
     def get_data(self, entry, metadata):
-        template = Template(self.data.decode(self.encoding))
-        name = entry.get('realname', entry.get('name'))
-        return template.render(metadata=metadata, name=name, path=name,
-                               source_path=name, repo=SETUP['repo'])
+        template = Template(self.data.decode(Bcfg2.Options.setup.encoding))
+        return template.render(
+            get_template_data(entry, metadata, self.name,
+                              default=DefaultJinja2DataProvider()))
     get_data.__doc__ = CfgGenerator.get_data.__doc__
