c7ecd5f504320ed3c9740c0512077351eb39657b
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2014-10-14 09:41:32 -0500

    testsuite: fix installation

--- testsuite/install.sh
+++ testsuite/install.sh
@@ -5,7 +5,7 @@
 sudo apt-get update -qq
 sudo apt-get install swig libxml2-utils
 
-pip install -r testsuite/requirements.txt --use-mirrors
+pip install -r testsuite/requirements.txt
 
 PYVER=$(python -c 'import sys;print(".".join(str(v) for v in sys.version_info[0:2]))')
 
@@ -14,13 +14,14 @@ if [[ ${PYVER:0:1} == "2" && $PYVER != "2.7" ]]; then
 fi
 
 if [[ "$WITH_OPTIONAL_DEPS" == "yes" ]]; then
-    pip install --use-mirrors PyYAML pyinotify boto pylibacl 'django<1.5' Jinja2
+    sudo apt-get install -y yum libaugeas0 augeas-lenses libacl1-dev libssl-dev
+
+    pip install --use-mirrors PyYAML pyinotify boto pylibacl 'django<1.5' \
+        Jinja2
     easy_install https://fedorahosted.org/released/python-augeas/python-augeas-0.4.1.tar.gz
     if [[ ${PYVER:0:1} == "2" ]]; then
         # django supports py3k, but South doesn't, and the django bits
         # in bcfg2 require South
-        sudo apt-get install -y yum libaugeas0 augeas-lenses libacl1-dev \
-            libssl-dev
         pip install cheetah 'South<0.8'
         pip install m2crypto
     fi
