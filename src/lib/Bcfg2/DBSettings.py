650623f8f5425ac4b629d19a1d4db79459d4f43e
Alexander Sulfrian <alexander@sulfrian.net>
2015-01-27 19:24:59 +0100

    DBSettings: Add possibility to specify arbitrary django options.

--- src/lib/Bcfg2/DBSettings.py
+++ src/lib/Bcfg2/DBSettings.py
@@ -123,6 +123,9 @@ def finalize_django_config(opts=None, silent=False):
             opts.web_prefix.rstrip('/') + \
             settings['MEDIA_URL']
 
+    if opts.django_settings:
+        settings.update(opts.django_settings)
+
     logger = logging.getLogger()
 
     logger.debug("Finalizing Django settings: %s" % settings)
@@ -265,7 +268,11 @@ class _OptionContainer(object):
         Bcfg2.Options.BooleanOption(
             cf=('reporting', 'web_debug'), help='Django debug'),
         Bcfg2.Options.Option(
-            cf=('reporting', 'web_prefix'), help='Web prefix')]
+            cf=('reporting', 'web_prefix'), help='Web prefix'),
+        Bcfg2.Options.Option(
+            cf=('reporting', 'django_settings'),
+            help='Additional django settings',
+            type=Bcfg2.Options.Types.literal_dict, default=dict())]
 
     @staticmethod
     def component_parsed_hook(opts):
