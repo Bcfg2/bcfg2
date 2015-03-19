Bcfg2/bcfg2

commit cae39b746051ff5f3257342d0659340283b2d6ef

Fix pylint errors
This also pins pylint to <= 0.28 so we don't have to keep playing
whack-a-mole with it.

Also removes unnecessary suppression of apt warnings. This is no
longer necessary in 12.04, so should be safe to remove. If you're on
Ubuntu < 12.04, upgrade for heaven's sake.

src/lib/Bcfg2/Client/Tools/APT.py
@@ -1,12 +1,10 @@
 """This is the Bcfg2 support for apt-get."""
 
-# suppress apt API warnings
-import warnings
-warnings.filterwarnings("ignore", "apt API not stable yet",
-                        FutureWarning)
-import apt.cache
 import os
 import sys
+
+import apt.cache
+
 import Bcfg2.Client.Tools
 
 
@@ -47,20 +45,20 @@ class APT(Bcfg2.Client.Tools.Tool):
                         for entry in struct
                         if entry.tag == 'Path' and
                         entry.get('type') == 'ignore']
-        self.__important__ = self.__important__ + \
-            ["%s/cache/debconf/config.dat" % self.var_path,
-             "%s/cache/debconf/templates.dat" % self.var_path,
-             '/etc/passwd', '/etc/group',
-             '%s/apt/apt.conf' % self.etc_path,
-             '%s/dpkg/dpkg.cfg' % self.etc_path] + \
+        self.__important__ = self.__important__ + [
+            "%s/cache/debconf/config.dat" % self.var_path,
+            "%s/cache/debconf/templates.dat" % self.var_path,
+            '/etc/passwd', '/etc/group',
+            '%s/apt/apt.conf' % self.etc_path,
+            '%s/dpkg/dpkg.cfg' % self.etc_path] + \
             [entry.get('name') for struct in config for entry in struct
-             if entry.tag == 'Path' and
-             entry.get('name').startswith(
-                 '%s/apt/sources.list' % self.etc_path)]
+             if (entry.tag == 'Path' and
+                 entry.get('name').startswith(
+                     '%s/apt/sources.list' % self.etc_path)]
         self.nonexistent = [entry.get('name') for struct in config
                             for entry in struct
-                            if entry.tag == 'Path' and
-                            entry.get('type') == 'nonexistent']
+                            if (entry.tag == 'Path' and
+                                entry.get('type') == 'nonexistent')]
         os.environ["DEBIAN_FRONTEND"] = 'noninteractive'
         self.actions = {}
         if self.setup['kevlar'] and not self.setup['dryrun']:

src/lib/Bcfg2/Client/Tools/MacPorts.py
@@ -42,10 +42,9 @@ class MacPorts(Bcfg2.Client.Tools.PkgTool):
             return False
 
         if entry.attrib['name'] in self.installed:
-            if (self.installed[entry.attrib['name']] == entry.attrib['version']
-                or entry.attrib['version'] == 'any'):
-                # if (not self.setup['quick'] and
-                #     entry.get('verify', 'true') == 'true'):
+            if (entry.attrib['version'] == 'any' or
+                    self.installed[entry.attrib['name']] ==
+                    entry.attrib['version']):
                 # FIXME: We should be able to check this once
                 #        http://trac.macports.org/ticket/15709 is implemented
                 return True

src/lib/Bcfg2/Client/Tools/POSIXUsers.py
@@ -146,8 +146,8 @@ class POSIXUsers(Bcfg2.Client.Tools.Tool):
         """ Get a list of supplmentary groups that the user in the
         given entry is a member of """
         return [g for g in self.existing['POSIXGroup'].values()
-                if entry.get("name") in g[3]
-                and self._in_managed_range('POSIXGroup', g[2])]
+                if entry.get("name") in g[3] and
+                self._in_managed_range('POSIXGroup', g[2])]
 
     def VerifyPOSIXUser(self, entry, _):
         """ Verify a POSIXUser entry """

src/lib/Bcfg2/Client/Tools/YUM.py
@@ -590,8 +590,8 @@ class YUM(Bcfg2.Client.Tools.PkgTool):
                                         "an RPM release")
                     continue
                 pkg_objs = [p for p in all_pkg_objs
-                            if (p.version == nevra['version']
-                                and p.release == nevra['release'])]
+                            if (p.version == nevra['version'] and
+                                p.release == nevra['release'])]
             else:
                 pkg_objs = self.yumbase.rpmdb.searchNevra(**short_yname(nevra))
             if len(pkg_objs) == 0:

src/lib/Bcfg2/Client/Tools/__init__.py
@@ -471,9 +471,9 @@ class PkgTool(Tool):
             # set all package states to true and flush workqueues
             pkgnames = [pkg.get('name') for pkg in packages]
             for entry in list(states.keys()):
-                if (entry.tag == 'Package'
-                    and entry.get('type') == self.pkgtype
-                    and entry.get('name') in pkgnames):
+                if (entry.tag == 'Package' and
+                        entry.get('type') == self.pkgtype and
+                        entry.get('name') in pkgnames):
                     self.logger.debug('Setting state to true for pkg %s' %
                                       entry.get('name'))
                     states[entry] = True
@@ -575,7 +575,7 @@ class SvcTool(Tool):
         return self.cmd.run(self.get_svc_command(service, 'stop'))
 
     def restart_service(self, service):
-        """ Restart a service.
+        """Restart a service.
 
         :param service: The service entry to modify
         :type service: lxml.etree._Element
@@ -608,8 +608,8 @@ class SvcTool(Tool):
             return
 
         for entry in bundle:
-            if (not self.handlesEntry(entry)
-                or not self._install_allowed(entry)):
+            if (not self.handlesEntry(entry) or
+                    not self._install_allowed(entry)):
                 continue
 
             estatus = entry.get('status')

src/lib/Bcfg2/Server/FileMonitor/__init__.py
@@ -336,8 +336,11 @@ class FileMonitor(Debuggable):
 available = dict()  # pylint: disable=C0103
 
 # TODO: loading the monitor drivers should be automatic
-from Bcfg2.Server.FileMonitor.Pseudo import Pseudo
-available['pseudo'] = Pseudo
+try:
+    from Bcfg2.Server.FileMonitor.Pseudo import Pseudo
+    available['pseudo'] = Pseudo
+except ImportError:
+    pass
 
 try:
     from Bcfg2.Server.FileMonitor.Fam import Fam

src/lib/Bcfg2/Server/Lint/Validate.py
@@ -1,12 +1,16 @@
-""" Ensure that all XML files in the Bcfg2 repository validate
-according to their respective schemas. """
+"""Validate XML files.
 
+Ensure that all XML files in the Bcfg2 repository validate according
+to their respective schemas.
+"""
+
+import glob
 import os
 import sys
-import glob
-import fnmatch
+
 import lxml.etree
 from subprocess import Popen, PIPE, STDOUT
+
 import Bcfg2.Server.Lint
 
 
@@ -204,17 +208,10 @@ class Validate(Bcfg2.Server.Lint.ServerlessPlugin):
         values are lists of the full paths to all files in the Bcfg2
         repository (or given with ``bcfg2-lint --stdin``) that match
         the glob."""
-        if self.files is not None:
-            listfiles = lambda p: fnmatch.filter(self.files,
-                                                 os.path.join('*', p))
-        else:
-            listfiles = lambda p: glob.glob(os.path.join(self.config['repo'],
-                                                         p))
-
         for path in self.filesets.keys():
             if '/**/' in path:
                 if self.files is not None:
-                    self.filelists[path] = listfiles(path)
+                    self.filelists[path] = self.list_matching_files(path)
                 else:  # self.files is None
                     fpath, fname = path.split('/**/')
                     self.filelists[path] = []
@@ -225,9 +222,9 @@ class Validate(Bcfg2.Server.Lint.ServerlessPlugin):
                                                      for f in files
                                                      if f == fname])
             else:
-                self.filelists[path] = listfiles(path)
+                self.filelists[path] = self.list_matching_files(path)
 
-        self.filelists['props'] = listfiles("Properties/*.xml")
+        self.filelists['props'] = self.list_matching_files("Properties/*.xml")
 
     def _load_schema(self, filename):
         """ Load an XML schema document, returning the Schema object

src/lib/Bcfg2/Server/Lint/ValidateJSON.py
@@ -1,11 +1,13 @@
-"""Ensure that all JSON files in the Bcfg2 repository are
+"""Validate JSON files.
+
+Ensure that all JSON files in the Bcfg2 repository are
 valid. Currently, the only plugins that uses JSON are Ohai and
-Properties."""
+Properties.
+"""
 
 import os
 import sys
-import glob
-import fnmatch
+
 import Bcfg2.Server.Lint
 
 try:
@@ -48,18 +50,11 @@ class ValidateJSON(Bcfg2.Server.Lint.ServerlessPlugin):
     def get_files(self):
         """Return a list of all JSON files to validate, based on
         :attr:`Bcfg2.Server.Lint.ValidateJSON.ValidateJSON.globs`. """
-        if self.files is not None:
-            listfiles = lambda p: fnmatch.filter(self.files,
-                                                 os.path.join('*', p))
-        else:
-            listfiles = lambda p: glob.glob(os.path.join(self.config['repo'],
-                                                         p))
-
         rv = []
         for path in self.globs:
             if '/**/' in path:
                 if self.files is not None:
-                    rv.extend(listfiles(path))
+                    rv.extend(self.list_matching_files(path))
                 else:  # self.files is None
                     fpath, fname = path.split('/**/')
                     for root, _, files in \
@@ -68,5 +63,5 @@ class ValidateJSON(Bcfg2.Server.Lint.ServerlessPlugin):
                         rv.extend([os.path.join(root, f)
                                    for f in files if f == fname])
             else:
-                rv.extend(listfiles(path))
+                rv.extend(self.list_matching_files(path))
         return rv

commit c8863b9a2dadacdacf433cbb818735c32e8f83a8

APT.py: Fix syntax error introduced in cae39b7
Signed-off-by: Sol Jerome <sol.jerome@gmail.com>

src/lib/Bcfg2/Client/Tools/APT.py
@@ -54,7 +54,7 @@ class APT(Bcfg2.Client.Tools.Tool):
             [entry.get('name') for struct in config for entry in struct
              if (entry.tag == 'Path' and
                  entry.get('name').startswith(
-                     '%s/apt/sources.list' % self.etc_path)]
+                     '%s/apt/sources.list' % self.etc_path))]
         self.nonexistent = [entry.get('name') for struct in config
                             for entry in struct
                             if (entry.tag == 'Path' and