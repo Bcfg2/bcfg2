Bcfg2/bcfg2

commit 89e7afbf74ffbbb54dd892bf2c4245aedee2a832

Remove blanket excepts from plugins and lint
This removes most blanket except: clauses from all plugins, including
the base plugin libraries, and bcfg2-lint. The few that remain should
all be necessary.

Most of the changes were quite minor, but this did require some
restructuring of the CfgPrivateKeyCreator; as a result, the tests for
that module were rewritten.

src/lib/Bcfg2/Server/Plugins/Bundler.py
@@ -92,10 +92,6 @@ class Bundler(Plugin,
                 self.logger.error("Bundler: Failed to render templated bundle "
                                   "%s: %s" % (bundlename, err))
                 continue
-            except:
-                self.logger.error("Bundler: Unexpected bundler error for %s" %
-                                  bundlename, exc_info=1)
-                continue
 
             if data.get("independent", "false").lower() == "true":
                 data.tag = "Independent"

src/lib/Bcfg2/Server/Lint/GroupPatterns.py
@@ -4,7 +4,8 @@
 import sys
 
 from Bcfg2.Server.Lint import ServerPlugin
-from Bcfg2.Server.Plugins.GroupPatterns import PatternMap
+from Bcfg2.Server.Plugins.GroupPatterns import PatternMap, \
+    PatternInitializationError
 
 
 class GroupPatterns(ServerPlugin):
@@ -36,7 +37,7 @@ class GroupPatterns(ServerPlugin):
                     PatternMap(pat, None, groups)
                 else:
                     PatternMap(None, pat, groups)
-            except:  # pylint: disable=W0702
+            except PatternInitializationError:
                 err = sys.exc_info()[1]
                 self.LintError("pattern-fails-to-initialize",
                                "Failed to initialize %s %s for %s: %s" %

src/lib/Bcfg2/Server/Lint/__init__.py
@@ -26,7 +26,7 @@ def _ioctl_GWINSZ(fd):  # pylint: disable=C0103
     from the given file descriptor """
     try:
         return struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ, '1234'))
-    except:  # pylint: disable=W0702
+    except (IOError, struct.error):
         return None
 
 
@@ -40,7 +40,7 @@ def get_termsize():
             fd = os.open(os.ctermid(), os.O_RDONLY)
             dims = _ioctl_GWINSZ(fd)
             os.close(fd)
-        except:  # pylint: disable=W0702
+        except IOError:
             pass
     if not dims:
         try:

src/lib/Bcfg2/Server/Plugin/helpers.py
@@ -266,7 +266,7 @@ class FileBacked(Debuggable):
         self.fam = Bcfg2.Server.FileMonitor.get_fam()
 
     def HandleEvent(self, event=None):
-        """ HandleEvent is called whenever the FAM registers an event.
+        """HandleEvent is called whenever the FAM registers an event.
 
         :param event: The event object
         :type event: Bcfg2.Server.FileMonitor.Event
@@ -276,13 +276,11 @@ class FileBacked(Debuggable):
             return
         try:
             self.data = open(self.name).read()
-            self.Index()
         except IOError:
             err = sys.exc_info()[1]
             self.logger.error("Failed to read file %s: %s" % (self.name, err))
-        except:
-            err = sys.exc_info()[1]
-            self.logger.error("Failed to parse file %s: %s" % (self.name, err))
+
+        self.Index()
 
     def Index(self):
         """ Index() is called by :func:`HandleEvent` every time the
@@ -1196,7 +1194,7 @@ class SpecificData(Debuggable):
             self.data = open(self.name).read()
         except UnicodeDecodeError:
             self.data = open(self.name, mode='rb').read()
-        except:  # pylint: disable=W0201
+        except IOError:
             self.logger.error("Failed to read file %s" % self.name)
 
 

src/lib/Bcfg2/Server/Plugin/interfaces.py
@@ -445,8 +445,14 @@ class ThreadedStatistics(Statistics, Threaded, threading.Thread):
             except Empty:
                 continue
             except:
-                err = sys.exc_info()[1]
-                self.logger.error("ThreadedStatistics: %s" % err)
+                # we want to catch all exceptions here so that a stray
+                # error doesn't kill the entire statistics thread. For
+                # instance, if a bad value gets pushed onto the queue
+                # and the assignment above raises TypeError, we want
+                # to report the error, ignore the bad value, and
+                # continue processing statistics.
+                self.logger.error("Unknown error processing statistics: %s" %
+                                  sys.exc_info()[1])
                 continue
             self.handle_statistic(client, xdata)
         if self.work_queue is not None and not self.work_queue.empty():

src/lib/Bcfg2/Server/Plugins/AWSTags.py
@@ -1,4 +1,4 @@
-""" Query tags from AWS via boto, optionally setting group membership """
+"""Query tags from AWS via boto, optionally setting group membership."""
 
 import os
 import re
@@ -82,22 +82,17 @@ class PatternFile(Bcfg2.Server.Plugin.XMLFileBacked):
                 self.tags.append(AWSTagPattern(entry.get("name"),
                                                entry.get("value"),
                                                groups))
-            except:  # pylint: disable=W0702
+            except re.error:
                 self.logger.error("AWSTags: Failed to initialize pattern %s: "
                                   "%s" % (entry.get("name"),
                                           sys.exc_info()[1]))
 
-    def get_groups(self, hostname, tags):
+    def get_groups(self, tags):
         """ return a list of groups that should be added to the given
-        client based on patterns that match the hostname """
+        client based on patterns that match the tags """
         ret = []
         for pattern in self.tags:
-            try:
-                ret.extend(pattern.get_groups(tags))
-            except:  # pylint: disable=W0702
-                self.logger.error("AWSTags: Failed to process pattern %s for "
-                                  "%s" % (pattern, hostname),
-                                  exc_info=1)
+            ret.extend(pattern.get_groups(tags))
         return ret
 
 
@@ -182,5 +177,4 @@ class AWSTags(Bcfg2.Server.Plugin.Plugin,
         return self.get_tags(metadata)
 
     def get_additional_groups(self, metadata):
-        return self.config.get_groups(metadata.hostname,
-                                      self.get_tags(metadata))
+        return self.config.get_groups(self.get_tags(metadata))

src/lib/Bcfg2/Server/Plugins/Cfg/CfgGenshiGenerator.py
@@ -10,6 +10,7 @@ from Bcfg2.Server.Plugin import PluginExecutionError, removecomment, \
     DefaultTemplateDataProvider, get_template_data
 from Bcfg2.Server.Plugins.Cfg import CfgGenerator
 from genshi.template import TemplateLoader, NewTextTemplate
+from genshi.template.base import TemplateError
 from genshi.template.eval import UndefinedError, Suite
 
 
@@ -117,6 +118,8 @@ class CfgGenshiGenerator(CfgGenerator):
                                                 quad[2]))
             raise
         except:
+            # this needs to be a blanket except, since it can catch
+            # any error raised by the genshi template.
             self._handle_genshi_exception(sys.exc_info())
     get_data.__doc__ = CfgGenerator.get_data.__doc__
 
@@ -184,10 +187,10 @@ class CfgGenshiGenerator(CfgGenerator):
     def handle_event(self, event):
         CfgGenerator.handle_event(self, event)
         try:
-            self.template = \
-                self.loader.load(self.name, cls=NewTextTemplate,
-                                 encoding=Bcfg2.Options.setup.encoding)
-        except:
+            self.template = self.loader.load(
+                self.name, cls=NewTextTemplate,
+                encoding=Bcfg2.Options.setup.encoding)
+        except TemplateError:
             raise PluginExecutionError("Failed to load template: %s" %
                                        sys.exc_info()[1])
     handle_event.__doc__ = CfgGenerator.handle_event.__doc__

src/lib/Bcfg2/Server/Plugins/Cfg/CfgPrivateKeyCreator.py
@@ -47,7 +47,7 @@ class CfgPrivateKeyCreator(XMLCfgCreator):
                      the given client metadata, and may be obtained by
                      doing ``self.XMLMatch(metadata)``
         :type spec: lxml.etree._Element
-        :returns: string - The filename of the private key
+        :returns: tuple - (private key data, public key data)
         """
         if spec is None:
             spec = self.XMLMatch(metadata)
@@ -91,10 +91,9 @@ class CfgPrivateKeyCreator(XMLCfgCreator):
                                     "with errors: %s" % (filename,
                                                          metadata.hostname,
                                                          result.stderr))
-            return filename
-        except:
+            return (open(filename).read(), open(filename + ".pub").read())
+        finally:
             shutil.rmtree(tempdir)
-            raise
 
     # pylint: disable=W0221
     def create_data(self, entry, metadata):
@@ -109,21 +108,17 @@ class CfgPrivateKeyCreator(XMLCfgCreator):
         """
         spec = self.XMLMatch(metadata)
         specificity = self.get_specificity(metadata)
-        filename = self._gen_keypair(metadata, spec)
+        privkey, pubkey = self._gen_keypair(metadata, spec)
 
-        try:
-            # write the public key, stripping the comment and
-            # replacing it with a comment that specifies the filename.
-            kdata = open(filename + ".pub").read().split()[:2]
-            kdata.append(self.pubkey_creator.get_filename(**specificity))
-            pubkey = " ".join(kdata) + "\n"
-            self.pubkey_creator.write_data(pubkey, **specificity)
+        # write the public key, stripping the comment and
+        # replacing it with a comment that specifies the filename.
+        kdata = pubkey.split()[:2]
+        kdata.append(self.pubkey_creator.get_filename(**specificity))
+        pubkey = " ".join(kdata) + "\n"
+        self.pubkey_creator.write_data(pubkey, **specificity)
 
-            # encrypt the private key, write to the proper place, and
-            # return it
-            privkey = open(filename).read()
-            self.write_data(privkey, **specificity)
-            return privkey
-        finally:
-            shutil.rmtree(os.path.dirname(filename))
+        # encrypt the private key, write to the proper place, and
+        # return it
+        self.write_data(privkey, **specificity)
+        return privkey
     # pylint: enable=W0221

commit 66d89ff62677c20139ea1973e547eea8e117a714

Bundler: Catch all exceptions in BuildStructures
This fixes a regression in 89e7afbf74ffb that made an unexpected
failure in a bundle (e.g., an arbitrary exception raised by a
templated bundle) prevent all subsequent bundles from being built,
too. Restoring the blanket 'except' makes it so that an unexpected
failure only affects the specific bundle that fails.

src/lib/Bcfg2/Server/Plugins/Bundler.py
@@ -92,6 +92,10 @@ class Bundler(Plugin,
                 self.logger.error("Bundler: Failed to render templated bundle "
                                   "%s: %s" % (bundlename, err))
                 continue
+            except:
+                self.logger.error("Bundler: Unexpected bundler error for %s" %
+                                  bundlename, exc_info=1)
+                continue
 
             if data.get("independent", "false").lower() == "true":
                 data.tag = "Independent"