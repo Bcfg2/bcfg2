Bcfg2/bcfg2

commit cd14868d4db8eaa7e9421e1d5fe8653294ac1e38

Options: migrated tools to new parser

tools/upgrade/1.3/migrate_info.py
@@ -5,7 +5,16 @@ import re
 import sys
 import lxml.etree
 import Bcfg2.Options
-from Bcfg2.Server.Plugin import INFO_REGEX
+
+INFO_REGEX = re.compile(r'owner:\s*(?P<owner>\S+)|' +
+                        r'group:\s*(?P<group>\S+)|' +
+                        r'mode:\s*(?P<mode>\w+)|' +
+                        r'secontext:\s*(?P<secontext>\S+)|' +
+                        r'paranoid:\s*(?P<paranoid>\S+)|' +
+                        r'sensitive:\s*(?P<sensitive>\S+)|' +
+                        r'encoding:\s*(?P<encoding>\S+)|' +
+                        r'important:\s*(?P<important>\S+)|' +
+                        r'mtime:\s*(?P<mtime>\w+)')
 
 
 PERMS_REGEX = re.compile(r'perms:\s*(?P<perms>\w+)')
@@ -32,16 +41,17 @@ def convert(info_file):
 
 
 def main():
-    opts = dict(repo=Bcfg2.Options.SERVER_REPOSITORY,
-                configfile=Bcfg2.Options.CFILE,
-                plugins=Bcfg2.Options.SERVER_PLUGINS)
-    setup = Bcfg2.Options.OptionParser(opts)
-    setup.parse(sys.argv[1:])
+    parser = Bcfg2.Options.get_parser(
+        description="Migrate from Bcfg2 1.2 info/:info files to 1.3 info.xml")
+    parser.add_options([Bcfg2.Options.Common.repository,
+                        Bcfg2.Options.Common.plugins])
+    parser.parse()
 
-    for plugin in setup['plugins']:
+    for plugin in Bcfg2.Options.setup.plugins:
         if plugin not in ['SSLCA', 'Cfg', 'TGenshi', 'TCheetah', 'SSHbase']:
             continue
-        for root, dirs, files in os.walk(os.path.join(setup['repo'], plugin)):
+        datastore = os.path.join(Bcfg2.Options.setup.repository, plugin)
+        for root, dirs, files in os.walk(datastore):
             for fname in files:
                 if fname in [":info", "info"]:
                     convert(os.path.join(root, fname))

tools/upgrade/1.3/migrate_perms_to_mode.py
@@ -54,16 +54,17 @@ def convertstructure(structfile):
 
 
 def main():
-    opts = dict(repo=Bcfg2.Options.SERVER_REPOSITORY,
-                configfile=Bcfg2.Options.CFILE,
-                plugins=Bcfg2.Options.SERVER_PLUGINS)
-    setup = Bcfg2.Options.OptionParser(opts)
-    setup.parse(sys.argv[1:])
-    repo = setup['repo']
+    parser = Bcfg2.Options.get_parser(
+        description="Migrate from Bcfg2 1.2 'perms' attribute to 1.3 'mode' "
+        "attribute")
+    parser.add_options([Bcfg2.Options.Common.repository,
+                        Bcfg2.Options.Common.plugins])
+    parser.parse()
+    repo = Bcfg2.Options.setup.repository
 
-    for plugin in setup['plugins']:
+    for plugin in Bcfg2.Options.setup.plugins:
         if plugin in ['Base', 'Bundler', 'Rules']:
-            for root, dirs, files in os.walk(os.path.join(repo, plugin)):
+            for root, _, files in os.walk(os.path.join(repo, plugin)):
                 for fname in files:
                     convertstructure(os.path.join(root, fname))
         if plugin not in ['Cfg', 'TGenshi', 'TCheetah', 'SSHbase', 'SSLCA']:

tools/bcfg2_local.py
@@ -6,19 +6,19 @@ the server core, then uses that to get probes, run them, and so on."""
 import sys
 import socket
 import Bcfg2.Options
-from Bcfg2.Client.Client import Client
-from Bcfg2.Server.Core import BaseCore
+from Bcfg2.Client import Client
+from Bcfg2.Server.Core import Core
 
 
-class LocalCore(BaseCore):
+class LocalCore(Core):
     """ Local server core similar to the one started by bcfg2-info """
 
-    def __init__(self, setup):
-        saved = (setup['syslog'], setup['logging'])
-        setup['syslog'] = False
-        setup['logging'] = None
-        Bcfg2.Server.Core.BaseCore.__init__(self, setup=setup)
-        setup['syslog'], setup['logging'] = saved
+    def __init__(self):
+        #saved = (setup['syslog'], setup['logging'])
+        #setup['syslog'] = False
+        #setup['logging'] = None
+        Bcfg2.Server.Core.BaseCore.__init__(self)
+        #setup['syslog'], setup['logging'] = saved
         self.load_plugins()
         self.fam.handle_events_in_interval(0.1)
 
@@ -57,26 +57,22 @@ class LocalClient(Client):
     """ A version of the Client class that uses LocalProxy instead of
     an XML-RPC proxy to make its calls """
 
-    def __init__(self, setup, proxy):
-        Client.__init__(self, setup)
+    def __init__(self, proxy):
+        Client.__init__(self)
         self._proxy = proxy
 
 
 def main():
-    optinfo = Bcfg2.Options.CLIENT_COMMON_OPTIONS
-    optinfo.update(Bcfg2.Options.SERVER_COMMON_OPTIONS)
-    if 'bundle_quick' in optinfo:
-        # CLIENT_BUNDLEQUICK option uses -Q, just like the server repo
-        # option.  the server repo is more important for this
-        # application.
-        optinfo['bundle_quick'] = Bcfg2.Options.Option('bundlequick',
-                                                       default=False)
-    setup = Bcfg2.Options.OptionParser(optinfo)
-    setup.parse(sys.argv[1:])
+    parser = Bcfg2.Options.Parser(
+        description="Run a Bcfg2 client against a local repository without a "
+        "server",
+        conflict_handler="resolve",
+        components=[LocalCore, LocalProxy, LocalClient])
+    parser.parse()
 
-    core = LocalCore(setup)
+    core = LocalCore()
     try:
-        LocalClient(setup, LocalProxy(core)).run()
+        LocalClient(LocalProxy(core)).run()
     finally:
         core.shutdown()
 

tools/posixusers_baseline.py
@@ -2,72 +2,46 @@
 
 import grp
 import sys
-import logging
 import lxml.etree
 import Bcfg2.Logger
+import Bcfg2.Options
 from Bcfg2.Client.Tools.POSIXUsers import POSIXUsers
-from Bcfg2.Options import OptionParser, Option, get_bool, CLIENT_COMMON_OPTIONS
 
 
-def get_setup():
-    optinfo = CLIENT_COMMON_OPTIONS
-    optinfo['nouids'] = Option("Do not include UID numbers for users",
-                               default=False,
-                               cmd='--no-uids',
-                               long_arg=True,
-                               cook=get_bool)
-    optinfo['nogids'] = Option("Do not include GID numbers for groups",
-                               default=False,
-                               cmd='--no-gids',
-                               long_arg=True,
-                               cook=get_bool)
-    setup = OptionParser(optinfo)
-    setup.parse(sys.argv[1:])
+class CLI(object):
+    options = [
+        Bcfg2.Options.BooleanOption(
+            "--no-uids", help="Do not include UID numbers for users"),
+        Bcfg2.Options.BooleanOption(
+            "--no-gids", help="Do not include GID numbers for groups")]
 
-    if setup['args']:
-        print("posixuser_[baseline.py takes no arguments, only options")
-        print(setup.buildHelpMessage())
-        raise SystemExit(1)
-    level = 30
-    if setup['verbose']:
-        level = 20
-    if setup['debug']:
-        level = 0
-    Bcfg2.Logger.setup_logging('posixusers_baseline.py',
-                               to_syslog=False,
-                               level=level,
-                               to_file=setup['logging'])
-    return setup
-
-
-def main():
-    setup = get_setup()
-    if setup['file']:
-        config = lxml.etree.parse(setup['file']).getroot()
-    else:
+    def __init__(self):
+        Bcfg2.Options.get_parser(
+            description="Generate a bundle with a baseline of POSIX users and "
+            "groups",
+            components=[self, POSIXUsers]).parse()
         config = lxml.etree.Element("Configuration")
-    users = POSIXUsers(logging.getLogger('posixusers_baseline.py'),
-                       setup, config)
+        self.users = POSIXUsers(config)
 
-    baseline = lxml.etree.Element("Bundle", name="posixusers_baseline")
-    for entry in users.FindExtra():
-        data = users.existing[entry.tag][entry.get("name")]
-        for attr, idx in users.attr_mapping[entry.tag].items():
-            if (entry.get(attr) or
-                (attr == 'uid' and setup['nouids']) or
-                (attr == 'gid' and setup['nogids'])):
-                continue
-            entry.set(attr, str(data[idx]))
-        if entry.tag == 'POSIXUser':
-            entry.set("group", grp.getgrgid(data[3])[0])
-            for group in users.user_supplementary_groups(entry):
-                memberof = lxml.etree.SubElement(entry, "MemberOf",
-                                                 group=group[0])
+    def run(self):
+        baseline = lxml.etree.Element("Bundle", name="posixusers_baseline")
+        for entry in self.users.FindExtra():
+            data = self.users.existing[entry.tag][entry.get("name")]
+            for attr, idx in self.users.attr_mapping[entry.tag].items():
+                if (entry.get(attr) or
+                    (attr == 'uid' and Bcfg2.Options.setup.no_uids) or
+                    (attr == 'gid' and Bcfg2.Options.setup.no_gids)):
+                    continue
+                entry.set(attr, str(data[idx]))
+            if entry.tag == 'POSIXUser':
+                entry.set("group", grp.getgrgid(data[3])[0])
+                for group in self.users.user_supplementary_groups(entry):
+                    lxml.etree.SubElement(entry, "MemberOf", group=group[0])
 
-        entry.tag = "Bound" + entry.tag
-        baseline.append(entry)
+            entry.tag = "Bound" + entry.tag
+            baseline.append(entry)
 
-    print(lxml.etree.tostring(baseline, pretty_print=True))
+        print(lxml.etree.tostring(baseline, pretty_print=True))
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(CLI().run())

tools/selinux_baseline.py
@@ -1,41 +1,18 @@
 #!/usr/bin/env python
 
 import sys
-import logging
 import lxml.etree
-
 import Bcfg2.Logger
 import Bcfg2.Options
-from Bcfg2.Client.Tools.SELinux import *
+from Bcfg2.Client.Tools.SELinux import SELinux
 
-LOGGER = None
-
-def get_setup():
-    global LOGGER
-    optinfo = Bcfg2.Options.CLIENT_COMMON_OPTIONS
-    setup = Bcfg2.Options.OptionParser(optinfo)
-    setup.parse(sys.argv[1:])
-
-    if setup['args']:
-        print("selinux_baseline.py takes no arguments, only options")
-        print(setup.buildHelpMessage())
-        raise SystemExit(1)
-    level = 30
-    if setup['verbose']:
-        level = 20
-    if setup['debug']:
-        level = 0
-    Bcfg2.Logger.setup_logging('selinux_base',
-                               to_syslog=False,
-                               level=level,
-                               to_file=setup['logging'])
-    LOGGER = logging.getLogger('bcfg2')
-    return setup
 
 def main():
-    setup = get_setup()
+    Bcfg2.Options.get_parser(
+        description="Get a baseline bundle of SELinux entries",
+        components=[SELinux]).parse()
     config = lxml.etree.Element("Configuration")
-    selinux = SELinux(LOGGER, setup, config)
+    selinux = SELinux(config)
 
     baseline = lxml.etree.Element("Bundle", name="selinux_baseline")
     for etype, handler in selinux.handlers.items():

tools/upgrade/1.1/posixunified.py
@@ -17,12 +17,13 @@ NOTE: This script takes a conservative approach when it comes to
 """
 
 if __name__ == '__main__':
-    opts = {
-               'repo': Bcfg2.Options.SERVER_REPOSITORY,
-           }
-    setup = Bcfg2.Options.OptionParser(opts)
-    setup.parse(sys.argv[1:])
-    repo = setup['repo']
+    parser = Bcfg2.Options.get_parser(
+        description="Migrate from Bcfg2 1.0-style POSIX entries to 1.1-style "
+        "unified Path entries")
+    parser.add_options([Bcfg2.Options.Common.repository])
+    parser.parse()
+
+    repo = Bcfg2.Options.setup.repository
     unifiedposixrules = "%s/Rules/unified-rules.xml" % repo
     rulesroot = lxml.etree.Element("Rules")
 

tools/upgrade/1.2/nagiosgen-convert.py
@@ -7,10 +7,13 @@ import lxml.etree
 import Bcfg2.Options
 
 def main():
-    opts = {'repo': Bcfg2.Options.SERVER_REPOSITORY}
-    setup = Bcfg2.Options.OptionParser(opts)
-    setup.parse(sys.argv[1:])
-    repo = setup['repo']
+    parser = Bcfg2.Options.get_parser(
+        description="Migrate from Bcfg2 1.1-style Properties-based NagiosGen "
+        "configuration to standalone 1.2-style")
+    parser.add_options([Bcfg2.Options.Common.repository])
+    parser.parse()
+
+    repo = Bcfg2.Options.setup.repository
     oldconfigfile = os.path.join(repo, 'Properties', 'NagiosGen.xml')
     newconfigpath = os.path.join(repo, 'NagiosGen')
     newconfigfile = os.path.join(newconfigpath, 'config.xml')
@@ -32,11 +35,11 @@ def main():
         if host.tag == lxml.etree.Comment:
             # skip comments
             continue
-        
+
         if host.tag == 'default':
             print("default tag will not be converted; use a suitable Group tag instead")
             continue
-        
+
         newhost = lxml.etree.Element("Client", name=host.tag)
         for opt in host:
             newopt = lxml.etree.Element("Option", name=opt.tag)

tools/upgrade/1.2/packages-convert.py
@@ -30,10 +30,13 @@ def place_source(xdata, source, groups):
     return xdata
 
 def main():
-    opts = {'repo': Bcfg2.Options.SERVER_REPOSITORY}
-    setup = Bcfg2.Options.OptionParser(opts)
-    setup.parse(sys.argv[1:])
-    repo = setup['repo']
+    parser = Bcfg2.Options.get_parser(
+        description="Migrate from Bcfg2 1.1-style Packages configuration to "
+        "1.2-style")
+    parser.add_options([Bcfg2.Options.Common.repository])
+    parser.parse()
+
+    repo = Bcfg2.Options.setup.repository
     configpath = os.path.join(repo, 'Packages')
     oldconfigfile  = os.path.join(configpath, 'config.xml')
     newconfigfile  = os.path.join(configpath, 'packages.conf')
@@ -78,7 +81,7 @@ def main():
             if el.tag == lxml.etree.Comment or el.tag == 'Config':
                 # skip comments and Config
                 continue
-        
+
             if el.tag == XI + 'include':
                 oldsources.append(os.path.join(configpath, el.get('href')))
                 newsource.append(el)
@@ -98,7 +101,7 @@ def main():
                     newel.set(tag.lower(), el.find(tag).text)
                 except AttributeError:
                     pass
-            
+
             for child in el.getchildren():
                 if child.tag in ['Component', 'Blacklist', 'Whitelist', 'Arch']:
                     newel.append(child)

commit 061640050936bd35e1d21aa44bc47237cca9a0f4

tools: Fix issue introduced in cd14868d with new parser for Options
The new parser creates the the full plugin classes out of the options,
so we need to use the __name__ attribute to get the name of the plugin
and the directory containing the files in the repository.

tools/upgrade/1.3/migrate_info.py
@@ -48,9 +48,11 @@ def main():
     parser.parse()
 
     for plugin in Bcfg2.Options.setup.plugins:
-        if plugin not in ['SSLCA', 'Cfg', 'TGenshi', 'TCheetah', 'SSHbase']:
+        plugin_name = plugin.__name__
+        if plugin_name not in ['SSLCA', 'Cfg', 'TGenshi', 'TCheetah',
+                               'SSHbase']:
             continue
-        datastore = os.path.join(Bcfg2.Options.setup.repository, plugin)
+        datastore = os.path.join(Bcfg2.Options.setup.repository, plugin_name)
         for root, dirs, files in os.walk(datastore):
             for fname in files:
                 if fname in [":info", "info"]:

tools/upgrade/1.3/migrate_perms_to_mode.py
@@ -71,17 +71,19 @@ def main():
     repo = Bcfg2.Options.setup.repository
 
     for plugin in Bcfg2.Options.setup.plugins:
-        if plugin in ['Base', 'Bundler', 'Rules']:
-            for root, _, files in os.walk(os.path.join(repo, plugin)):
+        plugin_name = plugin.__name__
+        if plugin_name in ['Base', 'Bundler', 'Rules']:
+            for root, _, files in os.walk(os.path.join(repo, plugin_name)):
                 if skip_path(root):
                     continue
                 for fname in files:
                     if skip_path(fname):
                         continue
                     convertstructure(os.path.join(root, fname))
-        if plugin not in ['Cfg', 'TGenshi', 'TCheetah', 'SSHbase', 'SSLCA']:
+        if plugin_name not in ['Cfg', 'TGenshi', 'TCheetah', 'SSHbase',
+                               'SSLCA']:
             continue
-        for root, dirs, files in os.walk(os.path.join(repo, plugin)):
+        for root, dirs, files in os.walk(os.path.join(repo, plugin_name)):
             if skip_path(root):
                 continue
             for fname in files: