2bbafa1499094a98a03fb5e84e5dff9b4b9a7aad
Alexander Sulfrian <alexander.sulfrian@fu-berlin.de>
2015-07-22 00:34:13 +0200

    Options: Add test to use config value for options from loaded components

--- testsuite/Testsrc/Testlib/TestOptions/Two.py
+++ testsuite/Testsrc/Testlib/TestOptions/Two.py
@@ -1,6 +1,7 @@
 """Test module for component loading."""
+from Bcfg2.Options import Option
 
 
 class Two(object):
     """Test class for component loading."""
-    pass
+    options = [Option('--test', cf=("config", "test"), dest="test", default="bar")]
