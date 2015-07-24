33e1a266c6abe716c024efc0338ff1f865685fbd
Alexander Sulfrian <alexander.sulfrian@fu-berlin.de>
2015-07-24 06:42:29 +0200

    Ldap: Complete renew of the Ldap plugin

--- src/lib/Bcfg2/Server/Plugins/Ldap.py
+++ src/lib/Bcfg2/Server/Plugins/Ldap.py
@@ -1,120 +1,132 @@
+""" A plugin to fetch data from a LDAP directory """
+
 import imp
-import logging
+import os
 import sys
 import time
 import traceback
+import Bcfg2.Options
 import Bcfg2.Server.Plugin
-
-logger = logging.getLogger('Bcfg2.Plugins.Ldap')
+from Bcfg2.Logger import Debuggable
+from Bcfg2.Utils import ClassName, safe_module_name
 
 try:
     import ldap
+    HAS_LDAP = True
 except ImportError:
-    logger.error("Unable to load ldap module. Is python-ldap installed?")
-    raise ImportError
-
-# time in seconds between retries after failed LDAP connection
-RETRY_DELAY = 5
-# how many times to try reaching the LDAP server if a connection is broken
-# at the very minimum, one retry is needed to handle a restarted LDAP daemon
-RETRY_COUNT = 3
-
-SCOPE_MAP = {
-    "base": ldap.SCOPE_BASE,
-    "one": ldap.SCOPE_ONELEVEL,
-    "sub": ldap.SCOPE_SUBTREE,
-}
-
-LDAP_QUERIES = []
-
-
-def register_query(query):
-    LDAP_QUERIES.append(query)
+    HAS_LDAP = False
 
 
 class ConfigFile(Bcfg2.Server.Plugin.FileBacked):
-    """
-    Config file for the Ldap plugin
+    """ Config file for the Ldap plugin  """
 
-    The config file cannot be 'parsed' in the traditional sense as we would
-    need some serious type checking ugliness to just get the LdapQuery
-    subclasses. The alternative would be to have the user create a list with
-    a predefined name that contains all queries.
-    The approach implemented here is having the user call a registering
-    decorator that updates a global variable in this module.
-    """
-    def __init__(self, filename):
-        self.filename = filename
-        Bcfg2.Server.Plugin.FileBacked.__init__(self, self.filename)
-        self.fam.AddMonitor(self.filename, self)
+    def __init__(self, name, core):
+        Bcfg2.Server.Plugin.FileBacked.__init__(self, name)
+        self.core = core
+        self.queries = list()
+        self.fam.AddMonitor(name, self)
 
     def Index(self):
-        """
-        Reregisters the queries in the config file
+        """ Get the queries from the config file """
+        try:
+            module = imp.load_source(safe_module_name('Ldap', self.name),
+                                     self.name)
+        except:  # pylint: disable=W0702
+            err = sys.exc_info()[1]
+            self.logger.error("Ldap: Failed to import %s: %s" %
+                              (self.name, err))
+            return
 
-        The config will take care of actually registering the queries,
-        so we just load it once and don't keep it.
-        """
-        global LDAP_QUERIES
-        LDAP_QUERIES = []
-        imp.load_source("ldap_cfg", self.filename)
+        if not hasattr(module, "__queries__"):
+            self.logger.error("Ldap: %s has no __queries__ list" % self.name)
+            return
+
+        self.queries = list()
+        for query in module.__queries__:
+            try:
+                self.queries.append(getattr(module, query))
+            except AttributeError:
+                self.logger.warning(
+                    "Ldap: %s exports %s, but has no such attribute" %
+                    (self.name, query))
+
+        if self.core.metadata_cache_mode in ['cautious', 'aggressive']:
+            self.core.metadata_cache.expire()
 
 
-class Ldap(Bcfg2.Server.Plugin.Plugin, Bcfg2.Server.Plugin.Connector):
-    """
-    The Ldap plugin allows adding data from an LDAP server to your metadata.
-    """
-    name = "Ldap"
+class Ldap(Bcfg2.Server.Plugin.Plugin,
+           Bcfg2.Server.Plugin.ClientRunHooks,
+           Bcfg2.Server.Plugin.Connector):
+    """ The Ldap plugin allows adding data from an LDAP server
+    to your metadata. """
+
     experimental = True
-    debug_flag = False
+
+    options = [
+        Bcfg2.Options.Option(
+            cf=('ldap', 'retries'), type=int, default=3,
+            help='The number of times to retry reaching the '
+                 'LDAP server if a connection is broken'),
+        Bcfg2.Options.Option(
+            cf=('ldap', 'retry_delay'), type=float, default=5.0,
+            help='The time in seconds betreen retries')]
 
     def __init__(self, core):
         Bcfg2.Server.Plugin.Plugin.__init__(self, core)
         Bcfg2.Server.Plugin.Connector.__init__(self)
-        self.config = ConfigFile(self.data + "/config.py")
 
-    def debug_log(self, message, flag=None):
-        if (flag is None) and self.debug_flag or flag:
-            self.logger.error(message)
+        if not HAS_LDAP:
+            msg = "Python ldap module is required for Ldap plugin"
+            self.logger.error(msg)
+            raise Bcfg2.Server.Plugin.PluginInitError(msg)
+
+        self.config = ConfigFile(os.path.join(self.data, 'config.py'))
 
     def get_additional_data(self, metadata):
         query = None
         try:
             data = {}
-            self.debug_log("LdapPlugin debug: found queries " +
-                           str(LDAP_QUERIES))
-            for QueryClass in LDAP_QUERIES:
-                query = QueryClass()
+            self.debug_log("Found queries %s" % self.config.queries)
+            for query_class in self.config.queries:
+                query = query_class()
                 if query.is_applicable(metadata):
-                    self.debug_log("LdapPlugin debug: processing query '" +
-                                   query.name + "'")
+                    self.debug_log("Processing query '%s'" % query.name)
                     data[query.name] = query.get_result(metadata)
                 else:
-                    self.debug_log("LdapPlugin debug: query '" + query.name +
-                                   "' not applicable to host '" +
-                                   metadata.hostname + "'")
+                    self.debug_log("query '%s' not applicable to host '%s'" %
+                                   (query.name, metadata.hostname))
             return data
-        except Exception:
+        except:  # pylint: disable=W0702
             if hasattr(query, "name"):
-                logger.error("LdapPlugin error: " +
-                             "Exception during processing of query named '" +
-                             str(query.name) +
-                             "', query results will be empty" +
-                             " and may cause bind failures")
-            for line in traceback.format_exception(sys.exc_info()[0],
-                                                   sys.exc_info()[1],
-                                                   sys.exc_info()[2]):
-                logger.error("LdapPlugin error: " +
-                             line.replace("\n", ""))
+                self.logger.error(
+                    "Exception during processing of query named '%s', query "
+                    "results will be empty and may cause bind failures" %
+                    query.name)
+            for line in traceback.format_exc().split('\n'):
+                self.logger.error(line)
             return {}
 
+    def start_client_run(self, metadata):
+        if self.core.metadata_cache_mode == 'aggressive':
+            self.logger.warning("Ldap is incompatible with aggressive "
+                                "client metadata caching, try 'cautious' "
+                                "or 'initial'")
+        self.core.metadata_cache.expire(metadata.hostname)
 
-class LdapConnection(object):
-    """
-    Connection to an LDAP server.
-    """
-    def __init__(self, host="localhost", port=389,
-                 binddn=None, bindpw=None):
+
+class LdapConnection(Debuggable):
+    """ Connection to an LDAP server. """
+
+    __scopes__ = {
+        'base': ldap.SCOPE_BASE,
+        'one': ldap.SCOPE_ONELEVEL,
+        'sub': ldap.SCOPE_SUBTREE,
+    }
+
+    def __init__(self, host="localhost", port=389, binddn=None,
+                 bindpw=None):
+        Debuggable.__init__(self)
+
         self.host = host
         self.port = port
         self.binddn = binddn
@@ -122,48 +134,62 @@ class LdapConnection(object):
         self.conn = None
 
     def __del__(self):
-        if self.conn:
-            self.conn.unbind()
+        """ Disconnection if the instance is destroyed. """
+        self.disconnect()
 
-    def init_conn(self):
+    def disconnect(self):
+        """ If a connection to an LDAP server is available, disconnect it. """
+        if self.conn:
+            self.conn.unbund()
+            self.conn = None
+
+    def connect(self):
+        """ Open a connection to the configured LDAP server, and do a simple
+        bind ff both binddn and bindpw are set. """
+        self.disconnect()
         self.conn = ldap.initialize(self.url)
         if self.binddn is not None and self.bindpw is not None:
             self.conn.simple_bind_s(self.binddn, self.bindpw)
 
     def run_query(self, query):
-        result = None
-        for attempt in range(RETRY_COUNT + 1):
-            if attempt >= 1:
-                logger.error("LdapPlugin error: " +
-                             "LDAP server down (retry " + str(attempt) + "/" +
-                             str(RETRY_COUNT) + ")")
+        """ Connect to the server and execute the query. If the server is
+        down, wait the configured amount and try to reconnect.
+
+        :param query: The query to execute on the LDAP server.
+        :type query: Bcfg.Server.Plugins.Ldap.LdapQuery
+        """
+        for attempt in range(Bcfg2.Options.setup.ldap_retries + 1):
             try:
                 if not self.conn:
-                    self.init_conn()
-                result = self.conn.search_s(
-                    query.base,
-                    SCOPE_MAP[query.scope],
-                    query.filter.replace("\\", "\\\\"),
-                    query.attrs,
-                )
-                break
+                    self.connect()
+
+                return self.conn.search_s(
+                    query.base, self.__scopes__[query.scope],
+                    query.filter.replace('\\', '\\\\'), query.attrs)
+
             except ldap.SERVER_DOWN:
                 self.conn = None
-                time.sleep(RETRY_DELAY)
-        return result
+                self.logger.error(
+                    "LdapConnection: Server %s down. Retry %d/%d in %.2fs." %
+                    (self.url, attempt + 1, Bcfg2.Options.setup.ldap_retries,
+                     Bcfg2.Options.setup.ldap_retry_delay))
+                time.sleep(Bcfg2.Options.setup.ldap_retry_delay)
+
+        return None
 
     @property
     def url(self):
-        return "ldap://" + self.host + ":" + str(self.port)
+        """ The URL of the LDAP server. """
+        return "ldap://%s:%d" % (self.host, self.port)
 
 
 class LdapQuery(object):
-    """
-    Query referencing an LdapConnection and providing several
-    methods for query manipulation.
-    """
+    """ Query referencing an LdapConnection and providing several
+    methods for query manipulation. """
 
-    name = "unknown"
+    #: Name of the Query, used to register it in additional data.
+    name = ClassName()
+
     base = ""
     scope = "sub"
     filter = "(objectClass=*)"
@@ -172,80 +198,48 @@ class LdapQuery(object):
     result = None
 
     def __unicode__(self):
-        return "LdapQuery:" + self.name
+        return "LdapQuery: %s" % self.name
 
-    def is_applicable(self, metadata):
-        """
-        Overrideable method to determine if the query is to be executed for
-        the given metadata object.
-        Defaults to true.
+    def is_applicable(self, metadata):  # pylint: disable=W0613
+        """ Check is the query should be executed for a given metadata
+        object.
+
+        :param metadata: The client metadata
+        :type metadata: Bcfg2.Server.Plugins.Metadata.ClientMetadata
         """
         return True
 
-    def prepare_query(self, metadata):
-        """
-        Overrideable method to alter the query based on metadata.
-        Defaults to doing nothing.
+    def prepare_query(self, metadata, **kwargs):  # pylint: disable=W0613
+        """ Prepares the query based on the client metadata. You can
+        for example modify the filter based on the client hostname.
 
-        In most cases, you will do something like
-
-            self.filter = "(cn=" + metadata.hostname + ")"
-
-        here.
+        :param metadata: The client metadata
+        :type metadata: Bcfg2.Server.Plugins.Metadata.ClientMetadata
         """
         pass
 
-    def process_result(self, metadata):
-        """
-        Overrideable method to post-process the query result.
-        Defaults to returning the unaltered result.
-        """
-        return self.result
+    def process_result(self, metadata, **kwargs):  # pylint: disable=W0613
+        """ Post-process the query result.
 
-    def get_result(self, metadata):
-        """
-        Method to handle preparing, executing and processing the query.
-        """
-        if isinstance(self.connection, LdapConnection):
-            self.prepare_query(metadata)
-            self.result = self.connection.run_query(self)
-            self.result = self.process_result(metadata)
-            return self.result
-        else:
-            logger.error("LdapPlugin error: " +
-                         "No valid connection defined for query " + str(self))
-            return None
-
-
-class LdapSubQuery(LdapQuery):
-    """
-    SubQueries are meant for internal use only and are not added
-    to the metadata object. They are useful for situations where
-    you need to run more than one query to obtain some data.
-    """
-    def prepare_query(self, metadata, **kwargs):
-        """
-        Overrideable method to alter the query based on metadata.
-        Defaults to doing nothing.
-        """
-        pass
-
-    def process_result(self, metadata, **kwargs):
-        """
-        Overrideable method to post-process the query result.
-        Defaults to returning the unaltered result.
+        :param metadata: The client metadata
+        :type metadata: Bcfg2.Server.Plugins.Metadata.ClientMetadata
         """
         return self.result
 
     def get_result(self, metadata, **kwargs):
+        """ Handle the perparation, execution and processing of the query.
+
+        :param metadata: The client metadata
+        :type metadata: Bcfg2.Server.Plugins.Metadata.ClientMetadata
+        :raises: :class:`Bcfg2.Server.Plugin.exceptions.PluginExecutionError`
         """
-        Method to handle preparing, executing and processing the query.
-        """
-        if isinstance(self.connection, LdapConnection):
+
+        if self.connection is not None:
             self.prepare_query(metadata, **kwargs)
             self.result = self.connection.run_query(self)
-            return self.process_result(metadata, **kwargs)
+            self.result = self.process_result(metadata, **kwargs)
         else:
-            logger.error("LdapPlugin error: " +
-                         "No valid connection defined for query " + str(self))
-            return None
+            raise Bcfg2.Server.Plugin.PluginExecutionError(
+                'No connection defined for %s' % self.name)
+
+        return self.result
