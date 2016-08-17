e12870f1913c8ce2069cdf7b95da72b55fb879cd
Alexander Sulfrian <alexander.sulfrian@fu-berlin.de>
2016-08-17 19:06:06 +0200

    Bcfg2/manage: Remove old code from the old options parsing

--- src/lib/Bcfg2/manage.py
+++ src/lib/Bcfg2/manage.py
@@ -14,17 +14,10 @@ parser = Bcfg2.Options.get_parser()
 parser.add_options([Bcfg2.Options.PositionalArgument('django_command', nargs='*')])
 parser.parse()
 
-if django.VERSION[0] == 1 and django.VERSION[1] <= 6:
-    try:
-        imp.find_module('settings') # Assumed to be in the same directory.
-    except ImportError:
-        import sys
-        sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
-        sys.exit(1)
-
 if __name__ == "__main__":
     if django.VERSION[0] == 1 and django.VERSION[1] >= 6:
         from django.core.management import execute_from_command_line
         execute_from_command_line(sys.argv[:1] + Bcfg2.Options.setup.django_command)
     else:
-        execute_manager(settings)
+        from django.core.management import execute_manager
+        execute_manager(Bcfg2.DBSettings.settings)
