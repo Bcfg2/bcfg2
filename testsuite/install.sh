b2fd02ad81f49cbd837afb589b140238fd028ff1
Alexander Sulfrian <alexander.sulfrian@fu-berlin.de>
2016-08-17 02:44:36 +0200

    travis-ci: Fix installation of dependencies
    This is just a quick fix: The new version of pip on travis does
    fail when using the --use-mirrors option (to be fair, the option
    was deprecated long time ago).

--- testsuite/install.sh
+++ testsuite/install.sh
@@ -10,13 +10,13 @@ pip install -r testsuite/requirements.txt
 PYVER=$(python -c 'import sys;print(".".join(str(v) for v in sys.version_info[0:2]))')
 
 if [[ ${PYVER:0:1} == "2" && $PYVER != "2.7" ]]; then
-    pip install --use-mirrors unittest2
+    pip install unittest2
 fi
 
 if [[ "$WITH_OPTIONAL_DEPS" == "yes" ]]; then
     sudo apt-get install -y yum libaugeas0 augeas-lenses libacl1-dev libssl-dev
 
-    pip install --use-mirrors PyYAML pyinotify boto pylibacl 'django<1.5' \
+    pip install PyYAML pyinotify boto pylibacl 'django<1.5' \
         Jinja2 mercurial guppy
     easy_install https://fedorahosted.org/released/python-augeas/python-augeas-0.4.1.tar.gz
     if [[ ${PYVER:0:1} == "2" ]]; then
