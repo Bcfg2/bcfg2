61aff9dea9c7e03bc712c9a2f43c6ae3f25b7848
Alexander Sulfrian <alexander.sulfrian@fu-berlin.de>
2016-08-17 02:44:36 +0200

    travis-ci: Fix installation of dependencies
    The new version of pip on travis does fail when using the --use-mirrors
    option (to be fair, the option was deprecated long time ago).

--- testsuite/install.sh
+++ testsuite/install.sh
@@ -2,13 +2,12 @@
 
 # install script for Travis-CI
 
-pip install -r testsuite/requirements.txt --use-mirrors
+pip install -r testsuite/requirements.txt
 
 PYVER=$(python -c 'import sys;print(".".join(str(v) for v in sys.version_info[0:2]))')
 
 if [[ "$WITH_OPTIONAL_DEPS" == "yes" ]]; then
-    pip install --use-mirrors genshi PyYAML pyinotify boto 'django<1.5' \
-        pylibacl
+    pip install genshi PyYAML pyinotify boto 'django<1.5' pylibacl
     easy_install https://fedorahosted.org/released/python-augeas/python-augeas-0.4.1.tar.gz
     if [[ ${PYVER:0:1} == "2" ]]; then
         # django supports py3k, but South doesn't, and the django bits
