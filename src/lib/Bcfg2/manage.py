14bd39d75e425b033a6996d9fffb1165edd7c4cd
Alexander Sulfrian <alexander.sulfrian@fu-berlin.de>
2016-08-17 16:48:10 +0200

    Bcfg2/manage: Fix option parsing

--- src/lib/Bcfg2/manage.py
+++ src/lib/Bcfg2/manage.py
@@ -5,7 +5,9 @@ import django
 import Bcfg2.Options
 import Bcfg2.DBSettings
 
-Bcfg2.Options.get_parser().parse()
+parser = Bcfg2.Options.get_parser()
+parser.add_options([Bcfg2.Options.PositionalArgument('django_command', nargs='*')])
+parser.parse()
 
 if django.VERSION[0] == 1 and django.VERSION[1] <= 6:
     try:
@@ -18,6 +20,6 @@ if django.VERSION[0] == 1 and django.VERSION[1] <= 6:
 if __name__ == "__main__":
     if django.VERSION[0] == 1 and django.VERSION[1] >= 7:
         from django.core.management import execute_from_command_line
-        execute_from_command_line(sys.argv)
+        execute_from_command_line(sys.argv[:1] + Bcfg2.Options.setup.django_command)
     else:
         execute_manager(settings)
