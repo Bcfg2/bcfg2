e0bc4708a3f7cc7675995177c85eb59fdb5dd677
Dmitri Iouchtchenko <johnnyspoon@gmail.com>
2016-09-11 00:42:46 -0400

    Packages/Pac: Parse dependencies

--- src/lib/Bcfg2/Server/Plugins/Packages/Pac.py
+++ src/lib/Bcfg2/Server/Plugins/Packages/Pac.py
@@ -1,8 +1,59 @@
 """ Pacman backend for :mod:`Bcfg2.Server.Plugins.Packages` """
 
+import os
 import tarfile
 from Bcfg2.Server.Plugins.Packages.Collection import Collection
 from Bcfg2.Server.Plugins.Packages.Source import Source
+
+
+def parse_db_file(pkgfile):
+    """ Parse a Pacman database file, returning a dictionary with
+    section headings for keys and lists of strings for values.
+    (Reference: ``sync_db_read`` in ``lib/libalpm/be_sync.c``)
+    """
+
+    pkg = {}
+    section = None
+
+    for line in pkgfile:
+        line = line.strip()
+
+        if section is not None:
+            if not line:
+                section = None
+            else:
+                pkg[section].append(line)
+        elif len(line) >= 2 and line[0] == line[-1] == '%':
+            section = line
+            pkg[section] = []
+
+    return pkg
+
+
+def parse_dep(dep):
+    """ Parse a Pacman dependency string, returning the package name,
+    version restriction (or ``None``), and description (or ``None``).
+    (Reference: ``alpm_dep_from_string`` in ``lib/libalpm/deps.c``)
+    """
+
+    rest_desc = dep.split(': ', 1)
+    if len(rest_desc) == 1:
+        rest, desc = rest_desc[0], None
+    else:
+        rest, desc = rest_desc
+
+    # Search for '=' last, since '<=' and '>=' are possible.
+    for symb in ['<', '>', '=']:
+        idx = rest.find(symb)
+        if idx >= 0:
+            name = rest[:idx]
+            version = rest[idx:]
+            break
+    else:
+        name = rest
+        version = None
+
+    return name, version, desc
 
 
 class PacCollection(Collection):
@@ -46,12 +97,9 @@ class PacSource(Source):
             raise Exception("PacSource : RAWUrl not supported (yet)")
 
     def read_files(self):
-        bdeps = dict()
-        bprov = dict()
-
-        depfnames = ['Depends', 'Pre-Depends']
-        if self.recommended:
-            depfnames.append('Recommends')
+        bdeps = {}
+        brecs = {}
+        bprov = {}
 
         for fname in self.files:
             if not self.rawurl:
@@ -62,8 +110,9 @@ class PacSource(Source):
                 barch = self.arches[0]
 
             if barch not in bdeps:
-                bdeps[barch] = dict()
-                bprov[barch] = dict()
+                bdeps[barch] = {}
+                brecs[barch] = {}
+                bprov[barch] = {}
             try:
                 self.debug_log("Packages: try to read %s" % fname)
                 tar = tarfile.open(fname, "r")
@@ -71,11 +120,39 @@ class PacSource(Source):
                 self.logger.error("Packages: Failed to read file %s" % fname)
                 raise
 
+            packages = {}
             for tarinfo in tar:
-                if tarinfo.isdir():
-                    self.pkgnames.add(tarinfo.name.rsplit("-", 2)[0])
-                    self.debug_log("Packages: added %s" %
-                                   tarinfo.name.rsplit("-", 2)[0])
+                if not tarinfo.isfile():
+                    continue
+                prefix = os.path.dirname(tarinfo.name)
+                if prefix not in packages:
+                    packages[prefix] = {}
+                pkg = parse_db_file(tar.extractfile(tarinfo))
+                packages[prefix].update(pkg)
+
+            for pkg in packages.values():
+                pkgname = pkg['%NAME%'][0]
+                self.pkgnames.add(pkgname)
+                bdeps[barch][pkgname] = []
+                brecs[barch][pkgname] = []
+
+                if '%DEPENDS%' in pkg:
+                    for dep in pkg['%DEPENDS%']:
+                        dname = parse_dep(dep)[0]
+                        bdeps[barch][pkgname].append(dname)
+
+                if '%OPTDEPENDS%' in pkg:
+                    for dep in pkg['%OPTDEPENDS%']:
+                        dname = parse_dep(dep)[0]
+                        brecs[barch][pkgname].append(dname)
+
+                if '%PROVIDES%' in pkg:
+                    for dep in pkg['%PROVIDES%']:
+                        dname = parse_dep(dep)[0]
+                        if dname not in bprov[barch]:
+                            bprov[barch][dname] = set()
+                        bprov[barch][dname].add(pkgname)
+
             tar.close()
-        self.process_files(bdeps, bprov)
+        self.process_files(bdeps, bprov, brecs)
     read_files.__doc__ = Source.read_files.__doc__
