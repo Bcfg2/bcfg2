Bcfg2/bcfg2

commit 590a1314bca44f29fc34bd27f0d49599b7eeab10

NagiosGen: Fix faulty configuration (from Marc Gariépy)
We previously added all configuration to the generated Nagios
configuration resulting in Nagios errors when parts of the configuration
were unused.

Signed-off-by: Sol Jerome <sol.jerome@gmail.com>

src/lib/Bcfg2/Server/Plugins/NagiosGen.py
@@ -104,9 +104,17 @@ class NagiosGen(Bcfg2.Server.Plugin.Plugin,
         for host in host_configs:
             host_data.append(open(host, 'r').read())
 
+        group_list = []
+        for line in "\n".join(host_data).splitlines():
+            # only include those groups which are actually used
+            if "hostgroup" in line:
+                group_list += line.split()[1].split(',')
+
+        group_list = list(set(group_list))
+
         for group in group_configs:
             group_name = re.sub("(-group.cfg|.*/(?=[^/]+))", "", group)
-            if "\n".join(host_data).find(group_name) != -1:
+            if group_name in group_list:
                 groupfile = open(group, 'r')
                 group_data.append(groupfile.read())
                 groupfile.close()

commit 11a57fdb320a08729eb28162e2d3fcee8a2e8a2a

Server/Plugins/NagiosGen: Always include default group
The default group file should be included, even if no server is in
the 'default' hostgroup. It allows to include global definitions
(like a service template).

The behaviour is documented and was broken by
590a1314bca44f29fc34bd27f0d49599b7eeab10.

src/lib/Bcfg2/Server/Plugins/NagiosGen.py
@@ -92,7 +92,7 @@ class NagiosGen(Plugin, Generator):
         for host in host_configs:
             host_data.append(open(host, 'r').read())
 
-        group_list = []
+        group_list = ['default']
         for line in "\n".join(host_data).splitlines():
             # only include those groups which are actually used
             if "hostgroup" in line: