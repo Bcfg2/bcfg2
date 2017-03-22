be3d9eb5cad2f09c395b24c55fd9c95c60caf561
Alexander Sulfrian <alexander.sulfrian@fu-berlin.de>
2017-03-22 20:53:12 +0100

    Bcfg2/manage.py: Fix pylint errors

--- src/lib/Bcfg2/manage.py
+++ src/lib/Bcfg2/manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import os
+""" Wrapper for the django manage.py with the Bcfg2 Opitons parsing. """
+
 import sys
 import django
 import Bcfg2.Options
@@ -10,14 +11,21 @@ try:
 except ImportError:
     pass
 
-parser = Bcfg2.Options.get_parser()
-parser.add_options([Bcfg2.Options.PositionalArgument('django_command', nargs='*')])
-parser.parse()
 
-if __name__ == "__main__":
+def main():
+    parser = Bcfg2.Options.get_parser()
+    parser.add_options([
+        Bcfg2.Options.PositionalArgument('django_command', nargs='*')])
+    parser.parse()
+
     if django.VERSION[0] == 1 and django.VERSION[1] >= 6:
         from django.core.management import execute_from_command_line
-        execute_from_command_line(sys.argv[:1] + Bcfg2.Options.setup.django_command)
+        execute_from_command_line(
+            sys.argv[:1] + Bcfg2.Options.setup.django_command)
     else:
         from django.core.management import execute_manager
         execute_manager(Bcfg2.DBSettings.settings)
+
+
+if __name__ == "__main__":
+    main()
